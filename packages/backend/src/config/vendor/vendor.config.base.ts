31cd917c01ceb7914e841bee844633a9d5924cf4
jeanphilippeds <jean-philippe@barooders.com>
2024-02-27 11:21:55 +0100

    :wrench: fix tch config

--- packages/backend/src/config/vendor/vendor.config.base.ts
+++ packages/backend/src/config/vendor/vendor.config.base.ts
@@ -588,11 +588,11 @@ export const baseVendorConfig: AllBaseVendorsConfig = {
         productTitle: 3,
         description: [38, 21],
         variantCondition: 4,
-        tags: [2, 3, 4, 6, 7, 9, 12],
-        images: [
-          13, 14, 15, 16, 17, 18, 19, 20, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-          31, 32, 33, 34, 35, 36, 37,
+        tags: [
+          2, 3, 4, 6, 7, 9, 12, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
+          34, 35, 36, 37,
         ],
+        images: [13, 14, 15, 16, 17, 18, 19, 20],
         option1: 8,
         compareAtPrice: 10,
         price: 11,
