a128eb45ea7001de6055bef4e4e198c177435a29
jeanphilippeds <jean-philippe@barooders.com>
2024-03-01 14:20:41 +0100

    :bug: fix cookie yes

--- packages/frontend/shared/document/scripts/GlobalScripts/index.tsx
+++ packages/frontend/shared/document/scripts/GlobalScripts/index.tsx
@@ -18,8 +18,26 @@ const GlobalScripts = () => {
 						dataLayer.push(arguments);
 					}
 					gtag('js', new Date());
-					gtag('config', '${config.gtag.id}');`,
+					gtag('config', '${config.gtag.id}');
+
+          gtag("consent", "default", {
+            ad_storage: "enabled",
+            ad_user_data: "enabled",
+            ad_personalization: "enabled",
+            analytics_storage: "enabled",
+            functionality_storage: "enabled",
+            personalization_storage: "enabled",
+            security_storage: "granted",
+            wait_for_update: 2000,
+          });
+          gtag("set", "ads_data_redaction", true);
+          gtag("set", "url_passthrough", true);`,
         }}
+      />
+      <Script
+        strategy="afterInteractive"
+        id="cookieyes"
+        src="https://cdn-cookieyes.com/client_data/abcc461482a9ff562819c1e8/script.js"
       />
     </>
   );
