7f0e9027d719daf2880fbf44ce755428938722d6
Clement Dessoude <clement@barooders.com>
2024-03-04 17:51:10 +0100

    🐛 Go back to menu after validating personal info

--- packages/frontend/shared/components/pages/SellingForm/UpdatePage/steps/PersonalInfoStep/index.tsx
+++ packages/frontend/shared/components/pages/SellingForm/UpdatePage/steps/PersonalInfoStep/index.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import PersonalInfoForm from '@/components/molecules/PersonalInfoForm/container';
+import useCurrentStep from '../../../_state/useCurrentStep';
 import { FormStepProps } from '../../../types';
 
 const PersonalInfoStep: React.FC<FormStepProps> = () => {
+  const { goBackToMenu } = useCurrentStep();
+
   return (
     <div className="p-5">
-      <PersonalInfoForm />
+      <PersonalInfoForm onSuccess={goBackToMenu} />
     </div>
   );
 };
