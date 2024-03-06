20328f44d8b51cf67257ba90cd5ca43fd1d18ce4
Clément Dessoude <clement.dessoude@gmail.com>
2024-03-06 18:47:01 +0100

    Revert "[MAIN] Replace url & mail & phone number inside description"

--- packages/dbt/macros/functions/replace_phone_numbers.sql
+++
@@ -1,7 +0,0 @@
-{% macro create_f_replace_phone_numbers() %}
-CREATE OR REPLACE FUNCTION REPLACE_PHONE_NUMBER(value varchar) RETURNS varchar AS $$
-        BEGIN
-            RETURN REGEXP_REPLACE(value, '(?:(?:\+|00)33[ ]?(?:\(0\)[ ]?)?|0){1}[1-9]{1}([ .-]?)(?:\d{2}\1?){3}\d{2}', '***', 1, 0);
-        END;
-$$ LANGUAGE plpgsql;
-{% endmacro %}
