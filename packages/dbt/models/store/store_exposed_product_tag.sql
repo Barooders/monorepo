b5fe98c8c04f82b641589b64236fe41cf06c14a8
jeanphilippeds <jean-philippe@barooders.com>
2024-03-06 20:27:10 +0100

    :sparkles: split on first :

--- packages/dbt/models/store/store_exposed_product_tag.sql
+++ packages/dbt/models/store/store_exposed_product_tag.sql
@@ -8,7 +8,7 @@ SELECT
   bp.id AS "product_id",
   split_part(t.value, ':',1) AS tag,
   t.value AS full_tag,
-  min(split_part(t.value, ':',2)) AS value
+  min(substring(t.value from position(':' in t.value) + 1)) AS value
 FROM fivetran_shopify.product_tag t
 JOIN {{ref('store_base_product')}} bp on bp."shopifyId" = t.product_id
 WHERE value LIKE '%:%'
