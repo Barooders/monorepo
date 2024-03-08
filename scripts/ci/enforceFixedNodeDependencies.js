768264942def8b728ee55e3b1a154dce5aa243eb
Clement Dessoude <clement@barooders.com>
2024-03-08 14:28:31 +0100

    chore: add script to check if dependencies are fixed

---
+++ scripts/ci/enforceFixedNodeDependencies.js
@@ -0,0 +1,62 @@
+const glob = require('glob');
+const path = require('path');
+
+const checkDependencies = (packageJsonFile) => {
+  const packageJson = require(packageJsonFile);
+  const { dependencies = {}, devDependencies = {} } = packageJson;
+
+  const areAllDependenciesFixed =
+    Object.entries(dependencies).every(hasFixedDependency);
+  const areAllDevDependenciesFixed =
+    Object.entries(devDependencies).every(hasFixedDependency);
+
+  if (!areAllDependenciesFixed || !areAllDevDependenciesFixed) {
+    console.error(`Some dependencies are not fixed for ${packageJsonFile}`);
+    return false;
+  }
+
+  return true;
+};
+
+const hasFixedDependency = ([name, version]) => {
+  const isNotFixed = version.includes('^') || version.includes('~');
+
+  if (isNotFixed) {
+    console.error(`⚠️  Dependency ${name} has unfixed version ${version}`);
+  }
+
+  return !isNotFixed;
+};
+
+const root = path.resolve(__dirname, '../..');
+
+glob(
+  root + '/**/package.json',
+  {
+    ignore: [
+      root + '/**/node_modules/**',
+      root + '/**/dist/**',
+      root + '/**/__generated/**',
+    ],
+  },
+  (err, files) => {
+    if (err) {
+      console.error('Error reading package.json files', err);
+    }
+
+    let areAllOk = true;
+
+    files.forEach((file) => {
+      console.log(`Checking ${file}...`);
+      const isOk = checkDependencies(file);
+
+      if (!isOk) {
+        areAllOk = false;
+      }
+    });
+
+    if (!areAllOk) {
+      process.exit(1);
+    }
+  },
+);
