946ceb7258d95ad010c520ba6782a3eb06351fab
Clement Dessoude <clement@barooders.com>
2024-03-12 18:22:16 +0100

    ⚡️ Add caching on product type retrieval from strapi

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,21 +1,22 @@
 import { ContextInterceptor } from '@libs/application/interceptors/context-interceptor';
 import { ExceptionInterceptor } from '@libs/application/interceptors/exception.interceptor';
+import { HealthCheckModule } from '@modules/__health-check/health-check.module';
 import { AuthModule } from '@modules/auth/auth.module';
 import { PaymentModule } from '@modules/buy__payment/module';
 import { ChatModule } from '@modules/chat/chat.module';
 import { CustomerModule } from '@modules/customer/customer.module';
 import { DeliveryProfileModule } from '@modules/delivery-profile/delivery-profile.module';
 import { OrderModule } from '@modules/order/order.module';
+import { PriceOfferModule } from '@modules/price-offer/price-offer.module';
 import { ProductModule } from '@modules/product/product.module';
 import { ShopifyAuthModule } from '@modules/shopify-auth/shopify-auth.module';
-import { HealthCheckModule } from '@modules/__health-check/health-check.module';
+import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { ScheduleModule } from '@nestjs/schedule';
 import { RequestContextModule } from 'nestjs-request-context';
 import { BaseModule } from './base.module';
-import { PriceOfferModule } from '@modules/price-offer/price-offer.module';
 
 const interceptors = [
   {
@@ -46,6 +47,7 @@ const applicationModules = [
     RequestContextModule,
     ScheduleModule.forRoot(),
     EventEmitterModule.forRoot(),
+    CacheModule.register(),
     BaseModule,
     ...applicationModules,
   ],
--- packages/backend/src/modules/product/infrastructure/pim/strapi.client.ts
+++ packages/backend/src/modules/product/infrastructure/pim/strapi.client.ts
@@ -1,19 +1,40 @@
 import { PIMProductType } from '@libs/domain/types';
 import { getPimProductTypesFromName } from '@libs/infrastructure/strapi/strapi.helper';
-import { BIKE_CATEGORY_ID as STRAPI_BIKE_CATEGORY_ID } from '@modules/product/constants';
+import {
+  PIM_PRODUCT_TYPE_CACHE_TTL_IN_MILLISECONDS,
+  BIKE_CATEGORY_ID as STRAPI_BIKE_CATEGORY_ID,
+} from '@modules/product/constants';
 import { IPIMClient } from '@modules/product/domain/ports/pim.client';
-import { Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 import { head } from 'lodash';
 
 @Injectable()
 export class StrapiClient implements IPIMClient {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
   async getPimProductType(productType: string): Promise<PIMProductType> {
+    const cachedPimProductType = await this.cacheManager.get<PIMProductType>(
+      `pim-product-type_${productType}`,
+    );
+
+    if (cachedPimProductType) {
+      return cachedPimProductType;
+    }
+
     const results = await getPimProductTypesFromName(productType);
     const firstMatch = head(results);
 
     if (!firstMatch) {
       throw new Error(`Product type ${productType} does not exist in PIM`);
     }
+
+    await this.cacheManager.set(
+      `pim-product-type_${productType}`,
+      firstMatch,
+      PIM_PRODUCT_TYPE_CACHE_TTL_IN_MILLISECONDS,
+    );
 
     return firstMatch;
   }
