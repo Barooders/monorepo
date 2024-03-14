78afcfe30ad316e2af034a8efb4a255120c4da9a
jeanphilippeds <jean-philippe@barooders.com>
2024-03-14 17:32:16 +0100

    :sparkles: allow orbitvu.co links

--- packages/dbt/macros/functions/replace_links_and_mails.sql
+++ packages/dbt/macros/functions/replace_links_and_mails.sql
@@ -2,7 +2,7 @@
 CREATE OR REPLACE FUNCTION REPLACE_LINKS_AND_MAILS(value varchar) RETURNS varchar AS $$
         BEGIN
             RETURN REGEXP_REPLACE(
-                REGEXP_REPLACE(value, '<a.*?>.*?</a>', '***', 'g'),
+                REGEXP_REPLACE(value, '<a(?!.*?orbitvu\.co).*?>.*?<\/a>', '***', 'g'),
                 '(https?://)?(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,255}\.(fr|com)([-a-zA-Z0-9()!@:%_+.~#?&/=]*)',
                 '***',
                 'g'
