15393ced6d379166e55d1cece7ccacc19bb98ffe
jeanphilippeds <jean-philippe@barooders.com>
2024-03-19 20:35:42 +0100

    :recycle: refactor type

--- packages/backend/src/libs/infrastructure/typesense/types.ts
+++ packages/backend/src/libs/infrastructure/typesense/types.ts
@@ -1,9 +1,24 @@
 import { DiscountRange } from '@libs/domain/types';
 
-export type SearchPublicVariantDocument = {
+type SearchBaseVariantDocument = {
   variant_shopify_id: number;
   variant_internal_id?: string;
   title: string;
+  product_type: string;
+  condition: string;
+  handle: string;
+  inventory_quantity: number;
+  array_tags: Record<string, string[]>;
+  price: number;
+  product_shopify_id: number;
+  product_internal_id: string;
+  product_image?: string;
+  updatedat_timestamp: number;
+  createdat_timestamp: number;
+  publishedat_timestamp: number;
+};
+
+export type SearchPublicVariantDocument = SearchBaseVariantDocument & {
   vendor: string;
   vendor_informations: {
     reviews: {
@@ -17,44 +32,16 @@ export type SearchPublicVariantDocument = {
       product_discount_range: DiscountRange;
     };
   };
-  product_type: string;
   variant_title: string;
   computed_scoring: number;
   is_refurbished?: string;
-  condition: string;
-  handle: string;
-  inventory_quantity: number;
-  array_tags: Record<string, string[]>;
-  price: number;
   discount: number;
-  product_shopify_id: number;
-  product_internal_id: string;
-  product_image?: string;
   compare_at_price: number;
   collection_internal_ids: string[];
   collection_handles: string[];
-  updatedat_timestamp: number;
-  createdat_timestamp: number;
-  publishedat_timestamp: number;
 };
 
-export type SearchB2BVariantDocument = {
-  variant_shopify_id: number;
-  variant_internal_id?: string;
-  title: string;
-  product_type: string;
-  condition: string;
-  handle: string;
-  inventory_quantity: number;
-  array_tags: Record<string, string[]>;
-  price: number;
-  product_shopify_id: number;
-  product_internal_id: string;
-  product_image?: string;
-  updatedat_timestamp: number;
-  createdat_timestamp: number;
-  publishedat_timestamp: number;
-};
+export type SearchB2BVariantDocument = SearchBaseVariantDocument;
 
 export type CollectionDocument = {
   collectionId: string;
