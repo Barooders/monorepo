38cfa79cb97de3d145eb58b01bea2adeb73d6d24
Nicolas Ngomai <nicolas.ngomai@gmail.com>
2024-03-19 16:47:18 +0100

    ✨ Add fastlap mapper to extract category

---
+++ packages/backend/src/modules/pro-vendor/infrastructure/api/woocommerce/mappers/fastlap.mapper.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { WooCommerceProduct } from '../types';
+import { WooCommerceDefaultMapper } from './default.mapper';
+
+@Injectable()
+export class FastlapMapper extends WooCommerceDefaultMapper {
+  getCategory(wooCommerceProduct: WooCommerceProduct): {
+    key: string;
+    name: string;
+  } {
+    const key = wooCommerceProduct.attributes
+      .find((a) => a.name.toLowerCase() === 'tipo')
+      ?.options.sort()
+      .join('-');
+
+    if (!key) {
+      return { key: 'unknown', name: 'unknown' };
+    }
+
+    return {
+      key,
+      name: key,
+    };
+  }
+}
