7ed0fbdf3d7e6893d181bf5fae29caea85940ca9
jeanphilippeds <jean-philippe@barooders.com>
2024-03-20 18:31:09 +0100

    :recycle: create B2B product card

--- packages/frontend/shared/components/pages/SearchPage/_components/B2BSearchResults.tsx
+++ packages/frontend/shared/components/pages/SearchPage/_components/B2BSearchResults.tsx
@@ -1,8 +1,4 @@
-import Button from '@/components/atoms/Button';
-import Link from '@/components/atoms/Link';
-import Characteristics from '@/components/molecules/ProductCard/_components/Characteristics';
-import ProductImage from '@/components/molecules/ProductCard/_components/ProductImage';
-import ProductPrice from '@/components/molecules/ProductCard/_components/ProductPrice';
+import B2BProductCard from '@/components/molecules/ProductCard/b2b';
 import { getDictionary } from '@/i18n/translate';
 import { fromSearchToB2BProductCard } from '@/mappers/fromSearchToProductCard';
 import { Hits, useInstantSearch } from 'react-instantsearch-hooks-web';
@@ -47,58 +43,8 @@ const B2BSearchResults: React.FC = () => {
           list: 'grid grid-cols-2 md:grid-cols-3 gap-x-2 gap-y-4',
         }}
         hitComponent={({ hit }: { hit: SearchB2BVariantDocument }) => {
-          const {
-            id,
-            image,
-            title,
-            tags,
-            productType,
-            variantCondition,
-            price,
-            stock,
-          } = fromSearchToB2BProductCard(hit);
-          return (
-            <Link href={'/'}>
-              <div className="grid w-full grid-cols-2 gap-1 overflow-hidden">
-                <div className="relative col-span-2 h-52 w-full flex-grow sm:h-64">
-                  {image && (
-                    <ProductImage
-                      image={image}
-                      labels={[]}
-                      discounts={[]}
-                    />
-                  )}
-                </div>
-                <div className="relative col-span-2 my-auto flex flex-grow flex-col">
-                  <Characteristics
-                    tags={tags}
-                    title={title}
-                    productType={productType}
-                    variantCondition={variantCondition}
-                    componentSize="medium"
-                  />
-                  <p className="mt-2 text-xs text-gray-600 lg:text-sm">
-                    Quantité disponible: {stock}
-                  </p>
-                  <div className="my-1">
-                    <ProductPrice
-                      productId={id}
-                      discounts={[]}
-                      compareAtPrice={price}
-                      price={price}
-                    />
-                  </div>
-                  <Button
-                    intent="primary"
-                    href="/"
-                    className="mt-2"
-                  >
-                    Faire une offre
-                  </Button>
-                </div>
-              </div>
-            </Link>
-          );
+          const productCardPropds = fromSearchToB2BProductCard(hit);
+          return <B2BProductCard {...productCardPropds} />;
         }}
       />
       {/* <div className="sticky bottom-10 left-0 right-0 mt-5 hidden justify-center md:flex">
