0badd369a91d5f629509bef1e41917d2e431ce1a
jeanphilippeds <jean-philippe@barooders.com>
2024-03-20 18:02:06 +0100

    :recycle: create base layout

--- packages/frontend/shared/components/layouts/main.tsx
+++ packages/frontend/shared/components/layouts/main.tsx
@@ -1,21 +1,10 @@
-import Footer from '@/components/molecules/Footer';
 import Header from '@/components/molecules/Header';
-import {
-  INNER_PAGE_BANNER_ANCHOR,
-  MODAL_ROOT_ANCHOR,
-  SNOWFALL_OVERLAY_ANCHOR,
-} from '@/config';
-import { poppins } from '@/document/fonts';
 import { metadataConfig, viewportConfig } from '@/document/metadata/global';
 import GlobalScripts from '@/document/scripts/GlobalScripts';
 import WebOnlyScripts from '@/document/scripts/WebOnlyScripts';
-import AnalyticsProvider from '@/providers/AnalyticsProvider';
-import FeatureFlagsProvider from '@/providers/FeatureFlagsProvider';
-import HasuraApolloProvider from '@/providers/HasuraApolloProvider';
-import { Toaster } from 'react-hot-toast';
-import { MdCheckCircle } from 'react-icons/md';
-import NoSSR from '../atoms/NoSSR';
+import Footer from '../molecules/Footer';
 import { MegaMenuChunk } from '../molecules/MegaMenu/shared/types/app/MegaMenu.types';
+import BaseLayout from './base';
 
 export const metadata = metadataConfig;
 export const viewport = viewportConfig;
@@ -30,40 +19,8 @@ const MainLayout: React.FC<PropsType> = ({ children, menu }) => {
     <>
       <GlobalScripts />
       <WebOnlyScripts />
-      <div className={`${poppins.className}`}>
-        <div
-          id={SNOWFALL_OVERLAY_ANCHOR}
-          className="pointer-events-none fixed z-100 h-full w-full"
-        />
-        <div id={MODAL_ROOT_ANCHOR} />
-
-        <FeatureFlagsProvider>
-          <HasuraApolloProvider>
-            <Header megaMenu={menu} />
-            <div id={INNER_PAGE_BANNER_ANCHOR} />
-            <AnalyticsProvider>{children}</AnalyticsProvider>
-          </HasuraApolloProvider>
-        </FeatureFlagsProvider>
-
-        <NoSSR>
-          <Toaster
-            position="bottom-right"
-            reverseOrder={false}
-            toastOptions={{
-              success: {
-                icon: <MdCheckCircle className="text-white" />,
-              },
-              style: {
-                color: '#fff',
-                borderRadius: '0px',
-                background: '#000',
-                width: 'fit-content',
-              },
-            }}
-          />
-        </NoSSR>
-        <Footer />
-      </div>
+      <BaseLayout header={<Header megaMenu={menu} />}>{children}</BaseLayout>
+      <Footer />
     </>
   );
 };
