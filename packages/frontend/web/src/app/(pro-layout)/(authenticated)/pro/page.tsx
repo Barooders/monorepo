2469d87a4d959143ffb7c954e31be509722ef3bd
jeanphilippeds <jean-philippe@barooders.com>
2024-03-20 14:59:59 +0100

    :sparkles: display results

--- packages/frontend/web/src/app/(pro-layout)/(authenticated)/pro/page.tsx
+++ packages/frontend/web/src/app/(pro-layout)/(authenticated)/pro/page.tsx
@@ -1,22 +1,41 @@
 'use client';
 
-import Filters from '@/components/pages/SearchPage/_components/Filters';
+import PageContainer from '@/components/atoms/PageContainer';
+import {
+  DesktopFilters,
+  MobileFilters,
+} from '@/components/pages/SearchPage/_components/Filters';
 import InstantSearchProvider from '@/components/pages/SearchPage/_components/InstantSearchProvider';
-import { InstantSearchSSRProvider } from 'react-instantsearch-hooks-web';
+import SearchResults from '@/components/pages/SearchPage/_components/SearchResults';
+import { Pagination } from 'react-instantsearch-hooks-web';
 
 const ProPage: React.FC = () => {
   return (
-    <>
-      <InstantSearchSSRProvider>
-        <InstantSearchProvider
-          filters={[]}
-          query={''}
-          ruleContexts={['']}
-        >
-          <Filters />
-        </InstantSearchProvider>
-      </InstantSearchSSRProvider>
-    </>
+    <PageContainer>
+      <InstantSearchProvider
+        filters={[]}
+        query={''}
+        ruleContexts={[]}
+      >
+        <div className="mt-1">
+          <div className="grid grid-cols-5 gap-10">
+            <div className="col-span-1 col-start-1 hidden lg:block">
+              <DesktopFilters />
+            </div>
+            <div className="col-span-5 flex flex-col gap-3 lg:col-span-4">
+              <div className="sticky top-[100px] z-10 flex bg-white pb-2 pt-1 lg:hidden">
+                <MobileFilters />
+              </div>
+              <SearchResults />
+              <Pagination />
+            </div>
+          </div>
+        </div>
+        {/* <div className="fixed right-6 bottom-3 md:hidden">
+        <SearchAlertButton />
+      </div> */}
+      </InstantSearchProvider>
+    </PageContainer>
   );
 };
 
