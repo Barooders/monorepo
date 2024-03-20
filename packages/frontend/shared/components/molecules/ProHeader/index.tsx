b6381b17f34a739a17da9d9271f21487838e4422
jeanphilippeds <jean-philippe@barooders.com>
2024-03-20 18:22:06 +0100

    :sparkles: add link to my account

--- packages/frontend/shared/components/molecules/ProHeader/index.tsx
+++ packages/frontend/shared/components/molecules/ProHeader/index.tsx
@@ -1,6 +1,8 @@
 import Link from '@/components/atoms/Link';
 import BaroodersLogo from '@/components/icons/BaroodersLogo';
 import { getDictionary } from '@/i18n/translate';
+import { MdOutlineAccountCircle } from 'react-icons/md';
+import HeaderButton from '../Header/_components/HeaderButton';
 
 const HeaderContainer: React.FC<{
   children: React.ReactNode;
@@ -20,13 +22,13 @@ const HeaderContainer: React.FC<{
 const ProHeader: React.FC = () => {
   const dictionnary = getDictionary('fr');
   return (
-    <>
-      <header
-        id="barooders-pro-header"
-        className="sticky top-0 z-30 flex flex-col items-center"
-      >
-        <HeaderContainer className="py-2 lg:py-4">
-          <div className="flex w-full flex-col items-center gap-2 lg:flex-row lg:gap-5">
+    <header
+      id="barooders-pro-header"
+      className="sticky top-0 z-30 flex flex-col items-center"
+    >
+      <HeaderContainer className="py-2 lg:py-4">
+        <div className="flex w-full flex-col items-center gap-2 lg:flex-row lg:gap-5">
+          <div className="flex w-full flex-col items-center justify-between gap-2 lg:flex-row lg:gap-5">
             <div
               className="flex w-[225px] flex-shrink-0 items-center"
               title={dictionnary.header.logo.title}
@@ -38,10 +40,18 @@ const ProHeader: React.FC = () => {
                 <BaroodersLogo className="fill-red-600" />
               </Link>
             </div>
+            <div className="hidden gap-1 lg:flex">
+              <HeaderButton
+                href="/pro/account"
+                title={dictionnary.header.icons.account}
+              >
+                <MdOutlineAccountCircle className="h-full w-full fill-secondary-900" />
+              </HeaderButton>
+            </div>
           </div>
-        </HeaderContainer>
-      </header>
-    </>
+        </div>
+      </HeaderContainer>
+    </header>
   );
 };
 
---
+++ packages/frontend/web/src/app/(pro-layout)/(authenticated)/pro/account/page.tsx
@@ -0,0 +1,5 @@
+import Account from '@/components/pages/Account';
+
+const AccountPage = () => <Account />;
+
+export default AccountPage;
