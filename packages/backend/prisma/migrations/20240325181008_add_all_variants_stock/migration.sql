ebb58e0c45ffabca2176ed0a088022905d0a5bda
jeanphilippeds <jean-philippe@barooders.com>
2024-03-25 19:21:13 +0100

    :sparkles: move properties at product level

---
+++ packages/backend/prisma/migrations/20240325181008_add_all_variants_stock/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Added the required column `inventory_quantity` to the `store_b2b_product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "dbt"."store_b2b_product" ADD COLUMN     "inventory_quantity" BIGINT NOT NULL;
