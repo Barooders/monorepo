3d294b6d0017530d2d6945c222eac9eb2439c231
jeanphilippeds <jean-philippe@barooders.com>
2024-03-25 14:41:24 +0100

    :card_file_box: add largest bundle price

---
+++ packages/backend/prisma/migrations/20240325134509_add_largest_bundle_price/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "dbt"."store_b2b_product_variant" ADD COLUMN     "largest_bundle_price" DOUBLE PRECISION;
--- packages/dbt/models/store/store_b2b_product_variant.sql
+++ packages/dbt/models/store/store_b2b_product_variant.sql
@@ -4,17 +4,31 @@
     pre_hook='delete from {{this}}'
 ) }}
 
+WITH largest_bundle_prices AS (
+    SELECT "productVariantId", "unitPriceInCents"
+    FROM (
+    SELECT 
+        "productVariantId",
+        "unitPriceInCents"
+        ROW_NUMBER() OVER (PARTITION BY "productVariantId" ORDER BY "minQuantity" DESC) AS row_num
+    FROM public."BundlePrice"
+    ) AS ranked
+    WHERE row_num = 1;
+)
+
 SELECT
     bpv."shopify_id" AS "shopify_id",
     pv.inventory_quantity AS "inventory_quantity",
     CURRENT_DATE AS "sync_date",
     pv.price,
     pv.compare_at_price AS "compare_at_price",
+    lbp."unitPriceInCents" AS "largest_bundle_price_in_cents",
     CAST(ppv.condition::TEXT AS dbt."Condition") as "condition",
     pv.updated_at AS "updated_at"
 
 FROM {{ref('store_base_product_variant')}} bpv
 LEFT JOIN public."ProductVariant" ppv ON ppv.id = bpv.id
+LEFT JOIN largest_bundle_prices lbp ON lbp."productVariantId" = bpv.id
 LEFT JOIN fivetran_shopify.product_variant pv ON pv.id = bpv."shopify_id"
 LEFT JOIN public."ProductSalesChannel" psc ON ppv."productId" = psc."productId"
 
