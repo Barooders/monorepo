cce1c19161980573a7f295ba4cab94105cc29234
jeanphilippeds <jean-philippe@barooders.com>
2024-03-26 13:26:58 +0100

    :sparkles: hide product page for b2b user

--- packages/frontend/shared/components/molecules/ProductCard/b2b.tsx
+++ packages/frontend/shared/components/molecules/ProductCard/b2b.tsx
@@ -1,7 +1,6 @@
 import Button from '@/components/atoms/Button';
 import { getDictionary } from '@/i18n/translate';
 import { formatCurrency } from '@/utils/currency';
-import Link from '../../atoms/Link';
 import Characteristics from './_components/Characteristics';
 import ProductImage from './_components/ProductImage';
 import ProductPrice from './_components/ProductPrice';
@@ -20,58 +19,55 @@ const B2BProductCard: React.FC<B2BProductCardProps> = ({
   largestBundlePrice,
   image,
   stock,
-  handle,
 }) => {
   return (
-    <Link href={`/products/${handle}`}>
-      <div className="grid w-full grid-cols-2 gap-1 overflow-hidden">
-        <div className="relative col-span-2 h-52 w-full flex-grow sm:h-64">
-          {image && (
-            <ProductImage
-              image={image}
-              labels={[
-                { content: <>x {stock}</>, position: 'right', color: 'purple' },
-              ]}
-              discounts={[]}
-            />
-          )}
-        </div>
-        <div className="relative col-span-2 my-auto flex min-h-[200px] flex-grow flex-col justify-between">
-          <Characteristics
-            tags={tags}
-            title={title}
-            productType={productType}
-            variantCondition={variantCondition}
-            componentSize="medium"
+    <div className="grid w-full grid-cols-2 gap-1 overflow-hidden">
+      <div className="relative col-span-2 h-52 w-full flex-grow sm:h-64">
+        {image && (
+          <ProductImage
+            image={image}
+            labels={[
+              { content: <>x {stock}</>, position: 'right', color: 'purple' },
+            ]}
+            discounts={[]}
           />
-          <div className="flex flex-col">
-            <div className="my-1">
-              {largestBundlePrice && (
-                <p className="text-xs text-gray-600 lg:text-sm">
-                  <span>{dict.b2b.productCard.largestBundlePrice}:</span>
-                  <span className="ml-1 font-bold">
-                    {formatCurrency(largestBundlePrice, { round: true })}€
-                  </span>
-                </p>
-              )}
-              <ProductPrice
-                productId={id}
-                discounts={[]}
-                compareAtPrice={compareAtPrice}
-                price={price}
-              />
-            </div>
-            <Button
-              intent="tertiary"
-              href="/"
-              className="mt-2"
-            >
-              {dict.b2b.productCard.makeAnOffer}
-            </Button>
+        )}
+      </div>
+      <div className="relative col-span-2 my-auto flex min-h-[200px] flex-grow flex-col justify-between">
+        <Characteristics
+          tags={tags}
+          title={title}
+          productType={productType}
+          variantCondition={variantCondition}
+          componentSize="medium"
+        />
+        <div className="flex flex-col">
+          <div className="my-1">
+            {largestBundlePrice && (
+              <p className="text-xs text-gray-600 lg:text-sm">
+                <span>{dict.b2b.productCard.largestBundlePrice}:</span>
+                <span className="ml-1 font-bold">
+                  {formatCurrency(largestBundlePrice, { round: true })}€
+                </span>
+              </p>
+            )}
+            <ProductPrice
+              productId={id}
+              discounts={[]}
+              compareAtPrice={compareAtPrice}
+              price={price}
+            />
           </div>
+          <Button
+            intent="tertiary"
+            href="/"
+            className="mt-2"
+          >
+            {dict.b2b.productCard.makeAnOffer}
+          </Button>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
