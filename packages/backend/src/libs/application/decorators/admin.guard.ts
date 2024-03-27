de02a1b468795aafc270cb4944632e5191caef0f
jeanphilippeds <jean-philippe@barooders.com>
2024-03-27 08:58:48 +0100

    :recycle: use shared hasura roles type

--- packages/backend/src/libs/application/decorators/admin.guard.ts
+++ packages/backend/src/libs/application/decorators/admin.guard.ts
@@ -1,6 +1,7 @@
 import { ExtractedUser } from '@modules/auth/domain/strategies/jwt/jwt.strategy';
 import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { HASURA_ROLES } from 'shared-types';
 
 @Injectable()
 export class AdminGuard extends AuthGuard('jwt') {
@@ -13,7 +14,7 @@ export class AdminGuard extends AuthGuard('jwt') {
   ) {
     const user = args[1] as ExtractedUser;
 
-    if (!user.roles.includes('admin')) {
+    if (!user.roles.includes(HASURA_ROLES.ADMIN)) {
       throw new UnauthorizedException('User does not have admin role');
     }
 
