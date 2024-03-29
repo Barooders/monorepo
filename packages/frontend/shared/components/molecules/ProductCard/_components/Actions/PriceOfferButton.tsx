f5e130aaef7b7ee1a8baca05980a1b6b37493ff1
Nicolas Ngomai <nicolas.ngomai@gmail.com>
2024-03-29 13:25:48 +0100

    🐛 Try to fix completed subscription

--- packages/frontend/shared/components/molecules/ProductCard/_components/Actions/PriceOfferButton.tsx
+++ packages/frontend/shared/components/molecules/ProductCard/_components/Actions/PriceOfferButton.tsx
@@ -9,6 +9,7 @@ import Modal from '@/components/atoms/Modal';
 import useUser from '@/hooks/state/useUser';
 import useIsLoggedIn from '@/hooks/useIsLoggedIn';
 import { getDictionary } from '@/i18n/translate';
+import { useTraceUpdate } from '@/utils/useTraceUpdate';
 import { gql, useSubscription } from '@apollo/client';
 import { first } from 'lodash';
 import React from 'react';
@@ -49,69 +50,96 @@ type PropsType = {
   shouldRedirectToChat?: boolean;
 };
 
-const PriceOfferButton: React.FC<PropsType> = ({
-  productId,
-  variant,
-  price,
-  buyerId,
-  negociationMaxAmountPercent,
-  className,
-  size,
-  shouldRedirectToChat,
-}) => {
-  const MakeOfferButton: React.FC<{
+const MakeOfferButton: React.FC<
+  PropsType & {
     openModal: () => void;
-  }> = ({ openModal }) => {
-    const { needsLogin } = useIsLoggedIn();
+  }
+> = ({ openModal, className, size }) => {
+  const { needsLogin } = useIsLoggedIn();
 
-    return (
-      <Button
-        className={`text-sm ${className}`}
-        intent="tertiary"
-        onClick={needsLogin(openModal)}
-        size={size}
-      >
-        {dict.makeOffer.openModal}
-      </Button>
-    );
-  };
+  return (
+    <Button
+      className={`text-sm ${className}`}
+      intent="tertiary"
+      onClick={needsLogin(openModal)}
+      size={size}
+    >
+      {dict.makeOffer.openModal}
+    </Button>
+  );
+};
 
-  const ButtonComponent: React.FC<{ openModal: () => void }> = ({
-    openModal,
-  }) => {
-    const { loading, data } =
-      useSubscription<OpenedPriceOfferProductPageSubscription>(
-        OPENED_PRICE_OFFERS_PRODUCT_PAGE,
-        {
-          variables: { productId },
+const ButtonComponent: React.FC<PropsType & { openModal: () => void }> = (
+  props,
+) => {
+  useTraceUpdate(props);
+  console.log('Re-render Button component');
+  const { productId, className, size } = props;
+  const { loading, data } =
+    useSubscription<OpenedPriceOfferProductPageSubscription>(
+      OPENED_PRICE_OFFERS_PRODUCT_PAGE,
+      {
+        variables: { productId },
+        onComplete() {
+          console.log('Completing websocket');
         },
-      );
-
-    const productShopifyId = first(data?.PriceOffer)?.product.shopifyId;
-
-    return loading ? (
-      <div className="flex items-start justify-center">
-        <Loader />
-      </div>
-    ) : productShopifyId ? (
-      <Button
-        className={`text-sm ${className}`}
-        intent="tertiary"
-        href={`/pages/chat?product=${productShopifyId}`}
-        size={size}
-      >
-        {dict.makeOffer.seePriceOffer}
-      </Button>
-    ) : (
-      <MakeOfferButton openModal={openModal} />
+        onData() {
+          console.log('Received data');
+        },
+        onError(e) {
+          console.error('Received error on websocket', e);
+        },
+      },
     );
-  };
 
+  const productShopifyId = first(data?.PriceOffer)?.product.shopifyId;
+
+  return loading ? (
+    <div className="flex items-start justify-center">
+      <Loader />
+    </div>
+  ) : productShopifyId ? (
+    <Button
+      className={`text-sm ${className}`}
+      intent="tertiary"
+      href={`/pages/chat?product=${productShopifyId}`}
+      size={size}
+    >
+      {dict.makeOffer.seePriceOffer}
+    </Button>
+  ) : (
+    <MakeOfferButton {...props} />
+  );
+};
+
+const PriceOfferButton: React.FC<PropsType> = (props) => {
+  const {
+    productId,
+    variant,
+    price,
+    buyerId,
+    negociationMaxAmountPercent,
+    shouldRedirectToChat,
+  } = props;
   const { hasuraToken } = useUser();
 
   return (
     <Modal
-      ButtonComponent={hasuraToken ? ButtonComponent : MakeOfferButton}
+      ButtonComponent={
+        hasuraToken
+          ? ({ openModal }) => (
+              <ButtonComponent
+                openModal={openModal}
+                {...props}
+              />
+            )
+          : ({ openModal }) => (
+              <MakeOfferButton
+                openModal={openModal}
+                {...props}
+              />
+            )
+      }
       ContentComponent={({ closeModal }) => (
         <MakeOfferModal
           originalPrice={price}
