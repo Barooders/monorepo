0fb0c0d1d0d200fbcda5d30e3fc20bce8bd38206
jeanphilippeds <jean-philippe@barooders.com>
2024-04-03 19:06:55 +0200

    :pencil2: use https

--- packages/backend/src/modules/customer/infrastructure/analytics/metabase.client.ts
+++ packages/backend/src/modules/customer/infrastructure/analytics/metabase.client.ts
@@ -2,15 +2,17 @@ import envConfig from '@config/env/env.config';
 import { PrismaMainClient } from '@libs/domain/prisma.main.client';
 import { UUID } from '@libs/domain/value-objects';
 import { IAnalyticsProvider } from '@modules/customer/domain/ports/analytics.provider';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import dayjs from 'dayjs';
 import { sign } from 'jsonwebtoken';
 
-const METABASE_SITE_URL = 'http://barooders-metabase.herokuapp.com';
+const METABASE_SITE_URL = 'https://barooders-metabase.herokuapp.com';
 const VENDOR_DASHBOARD_ID = 68;
 
 @Injectable()
 export class MetabaseClient implements IAnalyticsProvider {
+  private readonly logger: Logger = new Logger(MetabaseClient.name);
+
   constructor(private prisma: PrismaMainClient) {}
 
   async getVendorDataURL(vendorId: UUID): Promise<string> {
@@ -31,13 +33,11 @@ export class MetabaseClient implements IAnalyticsProvider {
       },
       exp: dayjs().add(10, 'minute').unix(),
     };
+
+    this.logger.warn('Metabase payload', payload);
+
     const token = sign(payload, envConfig.externalServices.metabase.secretKey);
 
-    return (
-      METABASE_SITE_URL +
-      '/embed/dashboard/' +
-      token +
-      '#bordered=true&titled=true'
-    );
+    return METABASE_SITE_URL + '/embed/dashboard/' + token + '#bordered=true';
   }
 }
