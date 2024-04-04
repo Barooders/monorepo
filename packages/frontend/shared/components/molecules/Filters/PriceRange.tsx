065751295beb9f2c86be0e133dfd231c08a3e476
jeanphilippeds <jean-philippe@barooders.com>
2024-04-04 15:40:51 +0200

    :sparkles: hide slider for total quantity

--- packages/frontend/shared/components/molecules/Filters/PriceRange.tsx
+++ packages/frontend/shared/components/molecules/Filters/PriceRange.tsx
@@ -13,7 +13,13 @@ const getLargeRange = (range: [number, number]) =>
     number,
   ];
 
-const PriceRange = ({ attribute }: { attribute: string }) => {
+const PriceRange = ({
+  attribute,
+  showSlider = true,
+}: {
+  attribute: string;
+  showSlider?: boolean;
+}) => {
   const { items } = useRefinementList({
     attribute,
     limit: config.search.maxValuesPerFacet,
@@ -82,49 +88,51 @@ const PriceRange = ({ attribute }: { attribute: string }) => {
   return (
     <div className="mb-3 flex flex-col gap-2">
       <p className="text-md font-bold">{getFacetLabel(attribute)}</p>
-      <div className="mt-3 w-full px-4 md:px-0">
-        <div className="relative flex h-14 w-full items-end gap-[1px]">
-          {priceSpreading.map((pricePoint, i) => (
-            <div
-              key={i}
-              className="flex-grow bg-slate-800"
-              style={{ height: `${pricePoint * 100}%` }}
-            />
-          ))}
-        </div>
-        <ReactSlider
-          min={0}
-          max={range.max}
-          className="flex items-center"
-          renderTrack={({ key, ...props }, state) => (
-            <div
-              key={key}
-              {...props}
-              className={`h-1 ${
-                state.index === 1 ? 'bg-slate-800' : 'bg-slate-300'
-              }`}
-            />
-          )}
-          renderThumb={({ key, className, ...props }) => (
-            <div
-              key={key}
-              className={`${className} flex w-0 items-center justify-center outline-0`}
-              {...props}
-            >
-              <div className="flex h-6 w-6 shrink-0 cursor-grab items-center justify-center rounded-full border-2 border-slate-800 bg-white">
-                <RxDragHandleVertical className="font-semibold " />
+      {showSlider && (
+        <div className="mt-3 w-full px-4 md:px-0">
+          <div className="relative flex h-14 w-full items-end gap-[1px]">
+            {priceSpreading.map((pricePoint, i) => (
+              <div
+                key={i}
+                className="flex-grow bg-slate-800"
+                style={{ height: `${pricePoint * 100}%` }}
+              />
+            ))}
+          </div>
+          <ReactSlider
+            min={0}
+            max={range.max}
+            className="flex items-center"
+            renderTrack={({ key, ...props }, state) => (
+              <div
+                key={key}
+                {...props}
+                className={`h-1 ${
+                  state.index === 1 ? 'bg-slate-800' : 'bg-slate-300'
+                }`}
+              />
+            )}
+            renderThumb={({ key, className, ...props }) => (
+              <div
+                key={key}
+                className={`${className} flex w-0 items-center justify-center outline-0`}
+                {...props}
+              >
+                <div className="flex h-6 w-6 shrink-0 cursor-grab items-center justify-center rounded-full border-2 border-slate-800 bg-white">
+                  <RxDragHandleVertical className="font-semibold " />
+                </div>
               </div>
-            </div>
-          )}
-          onChange={(newValue) => {
-            if (newValue[1] <= newValue[0]) return;
-            setPriceState(newValue);
-          }}
-          onAfterChange={updatePrice}
-          value={priceState}
-          step={10}
-        />
-      </div>
+            )}
+            onChange={(newValue) => {
+              if (newValue[1] <= newValue[0]) return;
+              setPriceState(newValue);
+            }}
+            onAfterChange={updatePrice}
+            value={priceState}
+            step={10}
+          />
+        </div>
+      )}
       <div className="mt-3 grid grid-cols-2 gap-2">
         <input
           type="number"
