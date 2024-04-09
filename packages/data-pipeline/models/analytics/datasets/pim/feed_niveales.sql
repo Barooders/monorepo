6ed0136abc9fbd204b09d15e773de45cea16488e
geoffroydautichamp <geoffroy.dautichamp@gmail.com>
2024-04-09 12:08:20 +0200

    fix title

--- packages/data-pipeline/models/analytics/datasets/pim/feed_niveales.sql
+++ packages/data-pipeline/models/analytics/datasets/pim/feed_niveales.sql
@@ -1,15 +1,18 @@
 {{ config(materialized='table') }}
 
 SELECT
-    f.id,
-    p.product_type,
+    f.variant_id id,
+    m.slug as type,
     f.brand,
     p.year as year,
-    f.gtin as ean,
-    f.description,
-    f.title as name,
-    COALESCE(f.sale_price, f.price) as price,
-    f.link,
-    f.image_link
-FROM {{ref('feed_gmc_api')}} AS f
-JOIN {{ref('dim_product')}} AS p on p.id = f.item_group_id
+    f.barcode as ean,
+    f_api.description,
+    p.modele as name,
+    f.title_proper as title,
+    f.price,
+    f_api.link,
+    f.image_url_1 as image_link
+FROM {{ref('feed_gmc')}} AS f
+JOIN {{ref('feed_gmc_api')}} AS f_api on f_api.id = f.variant_id
+JOIN {{ref('dim_product')}} AS p on p.id = f.product_id
+JOIN config.niveales_mapping as m on m.product_type = p.product_type
