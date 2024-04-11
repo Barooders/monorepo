0e878251e3824e5559d25c35de6708b5f68c7fbc
geoffroydautichamp <geoffroy.dautichamp@gmail.com>
2024-04-11 11:12:03 +0200

    error check for modele and title

--- packages/data-pipeline/models/analytics/datasets/pim/feed_niveales.sql
+++ packages/data-pipeline/models/analytics/datasets/pim/feed_niveales.sql
@@ -21,3 +21,4 @@ FROM {{ref('feed_gmc')}} AS f
 JOIN {{ref('feed_gmc_api')}} AS f_api on f_api.id = f.variant_id
 JOIN {{ref('dim_product')}} AS p on p.id = f.product_id
 JOIN config.niveales_mapping as m on m.product_type = p.product_type
+where not (p.title is null and p.modele is null)
