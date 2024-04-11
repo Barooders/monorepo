2f2b1e72f088c96f1fe68541e858cd52dc3f329f
jeanphilippeds <jean-philippe@barooders.com>
2024-04-11 18:11:06 +0200

    :recycle: clean render script

--- install-for-render.sh
+++ install-for-render.sh
@@ -6,18 +6,21 @@ PACKAGES=$(ls packages)
 
 extract_cached_dependencies() {
     source_dir="$1"
-    dest_dir="packages/$1/node_modules/"
-    mkdir -p "$dest_dir"  # Ensure destination directory exists
-    mv "$subdirectories_nodemodules/$source_dir/" "$dest_dir" || true
+    package_dir="packages/$1/node_modules/"
+    cache_dir="$subdirectories_nodemodules/$source_dir/"
+
+    mkdir -p $package_dir  # Ensure destination directory exists
+    mv $cache_dir $package_dir || true
 }
 
 cache_dependencies() {
     source_dir="$1"
     package_dir="packages/$1/node_modules/"
-    dest_dir="$subdirectories_nodemodules/$source_dir"
-    rm -rf "$dest_dir"  # Remove existing cache
-    mkdir -p "$dest_dir"  # Ensure destination directory exists
-    cp -r "$package_dir" "$subdirectories_nodemodules/$source_dir" || true
+    cache_dir="$subdirectories_nodemodules/$source_dir"
+
+    rm -rf $cache_dir  # Remove existing cache
+    mkdir -p $cache_dir  # Ensure destination directory exists
+    cp -r $package_dir $cache_dir || true
 }
 
 # Move modules to package directories
