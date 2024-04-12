ab2b8d4104a8577866c85d8cc055d400bbe9cc62
jeanphilippeds <jean-philippe@barooders.com>
2024-04-12 11:50:34 +0200

    :sparkles: trigger job on render

--- packages/backend/src/cron.ts
+++ packages/backend/src/cron.ts
@@ -2,6 +2,7 @@
 import 'dotenv.config';
 
 import envConfig from '@config/env/env.config';
+import { jsonStringify } from '@libs/helpers/json';
 import { schedule } from 'node-cron';
 
 const {
@@ -12,7 +13,26 @@ const {
 console.log(`Starting ${jobs.length} cron jobs`);
 
 jobs.forEach(({ command, cron }) => {
-  schedule(cron, () => {
-    console.log(`Running command: ${command} as requested by: ${cron}`);
+  schedule(cron, async () => {
+    try {
+      const result = await fetch(endpoint, {
+        method: 'POST',
+        headers: new Headers([
+          ['Content-Type', 'application/json'],
+          ['Authorization', `Bearer ${bearerToken}`],
+        ]),
+        body: jsonStringify({ startCommand: command }),
+      });
+
+      if (!result.ok) {
+        console.error(
+          `Failed create job with command: ${command}. Status: ${result.status}`,
+        );
+      }
+
+      console.log(`Running command: ${command} as requested by: ${cron}`);
+    } catch (error) {
+      console.error(`An unexpected error occured:`, error);
+    }
   });
 });
