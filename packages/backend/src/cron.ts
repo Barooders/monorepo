3212b2fb3dc25919d846d56256b4b41a76aeaae9
jeanphilippeds <jean-philippe@barooders.com>
2024-04-12 11:33:41 +0200

    :sparkles: loop on cronjobs

--- packages/backend/src/cron.ts
+++ packages/backend/src/cron.ts
@@ -1,5 +1,13 @@
+/* eslint-disable no-console */
+import 'dotenv.config';
+
+import envConfig from '@config/env/env.config';
 import { schedule } from 'node-cron';
 
-schedule('* * * * *', () => {
-  console.log('running a task every minute');
+console.log('Starting cron jobs');
+
+envConfig.cronJobs.forEach(({ cron, command }) => {
+  schedule(cron, () => {
+    console.log(`Running command: ${command} as requested by: ${cron}`);
+  });
 });
