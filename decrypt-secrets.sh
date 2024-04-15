ed6548ff01c0f68220a739fd498fab370de76af9
Nicolas Ngomai <nicolas.ngomai@gmail.com>
2024-04-15 15:42:17 +0200

    🐛 Decrypt secret on store deploy

---
+++ decrypt-secrets.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source .env &> /dev/null || true
+
+if [ -z $TRANSCRYPT_PASSWORD ]
+then
+	echo 'You should export a $TRANSCRYPT_PASSWORD. If you do not have it, ask someone to run ./bin/transcrypt -d'
+	exit 1
+fi
+echo "Diff files:"
+git diff --name-only | grep ''
+echo " "
+echo "Decrypting secrets..."
+./bin/transcrypt -y -c aes-256-cbc -p $TRANSCRYPT_PASSWORD || true
