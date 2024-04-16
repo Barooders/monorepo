597c40550aa3b3eef0086f9f45450049a1ec3fcf
Nicolas Ngomai <nicolas.ngomai@gmail.com>
2024-04-16 10:23:49 +0200

    ✨ Reduce updateStatuses frequency

--- packages/backend/src/config/vendor/vendor.config.prod.ts
+++ packages/backend/src/config/vendor/vendor.config.prod.ts
@@ -6,7 +6,7 @@ export const prodVendorConfig: EnvVendorsConfig = {
     synchros: [
       { commandName: 'syncProducts', cron: '0 6 * * *' },
       { commandName: 'syncProducts', cron: '0 18 * * *' },
-      //{ commandName: 'updateProductStatuses', cron: '20 * * * *' },
+      // { commandName: 'updateProductStatuses', cron: '20 */2 * * *' },
     ],
   },
   freeglisse: {
@@ -20,14 +20,14 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: 'dfb30f4b-f6be-48f4-a94f-26749d93d4e4',
     synchros: [
       { commandName: 'syncProducts', cron: '0 1 * * *' },
-      { commandName: 'updateProductStatuses', cron: '20 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '20 */3 * * *' },
     ],
   },
   bike_xtreme: {
     vendorId: 'c789ebfe-f2c7-4dc5-a75d-47785d99b30c',
     synchros: [
       { commandName: 'syncProducts', cron: '30 2 * * *' },
-      { commandName: 'updateProductStatuses', cron: '30 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '30 */3 * * *' },
     ],
   },
   velo_emotion: {
@@ -53,14 +53,14 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: '3ac82924-0bb3-4ede-a5fe-81d09df0fa39',
     synchros: [
       { commandName: 'syncProducts', cron: '0 23 * * *' },
-      { commandName: 'updateProductStatuses', cron: '50 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '50 */2 * * *' },
     ],
   },
   semotion: {
     vendorId: '8f56e505-cb73-44f0-b53f-83c0340e0ee8',
     synchros: [
       { commandName: 'syncProducts', cron: '30 7 * * *' },
-      { commandName: 'updateProductStatuses', cron: '0 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '0 */3 * * *' },
     ],
   },
   gary_bom: { vendorId: '09cf6248-127d-4be6-a6ab-a8583d92a5b5', synchros: [] },
@@ -68,7 +68,7 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: 'a528a855-ebc4-47bb-9e3c-e932ccf63b36',
     synchros: [
       { commandName: 'syncProducts', cron: '0 3 * * *' },
-      { commandName: 'updateProductStatuses', cron: '40 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '40 */3 * * *' },
     ],
   },
   paname_bicis: {
@@ -87,70 +87,70 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: '7905706f-2bb2-43a5-b1fb-fb315139d3de',
     synchros: [
       { commandName: 'syncProducts', cron: '0 0 * * *' },
-      { commandName: 'updateProductStatuses', cron: '30 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '30 */3 * * *' },
     ],
   },
   funbike: {
     vendorId: '8ff695e6-f0f8-4710-9b6f-908bcef9cd06',
     synchros: [
       { commandName: 'syncProducts', cron: '0 0 * * *' },
-      { commandName: 'updateProductStatuses', cron: '10 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '10 */3 * * *' },
     ],
   },
   ebs: {
     vendorId: 'd32c05d6-6ea2-4e6b-abb3-62e7217e94ac',
     synchros: [
       { commandName: 'syncProducts', cron: '0 0 * * *' },
-      { commandName: 'updateProductStatuses', cron: '0 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '0 */3 * * *' },
     ],
   },
   trocsport: {
     vendorId: '0d452628-7292-4192-9641-f2039df89303',
     synchros: [
       { commandName: 'syncProducts', cron: '0 2 * * *' },
-      { commandName: 'updateProductStatuses', cron: '50 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '50 */3 * * *' },
     ],
   },
   bcycles: {
     vendorId: '1e24d345-07cc-4328-94e7-6dbf6c2c1050',
     synchros: [
       { commandName: 'syncProducts', cron: '30 7 * * *' },
-      { commandName: 'updateProductStatuses', cron: '50 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '50 */3 * * *' },
     ],
   },
   bewak: {
     vendorId: '94ec432e-cbd8-45cf-955d-e3d67a8e3c0a',
     synchros: [
       { commandName: 'syncProducts', cron: '0 1 * * *' },
-      { commandName: 'updateProductStatuses', cron: '50 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '50 */2 * * *' },
     ],
   },
   bewak_excluded_brands: {
     vendorId: '43b03898-6866-42ab-81cc-8faacbd07ab0',
     synchros: [
       { commandName: 'syncProducts', cron: '30 2 * * *' },
-      { commandName: 'updateProductStatuses', cron: '50 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '50 */2 * * *' },
     ],
   },
   horizons_angers: {
     vendorId: 'b131f9d2-90f6-4022-9684-a24d79e60c62',
     synchros: [
       { commandName: 'syncProducts', cron: '0 0 * * *' },
-      { commandName: 'updateProductStatuses', cron: '50 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '50 */3 * * *' },
     ],
   },
   kite_spirit: {
     vendorId: '34ce035e-55d4-4cf8-a6ae-4dec5edda27c',
     synchros: [
       { commandName: 'syncProducts', cron: '0 23 * * *' },
-      { commandName: 'updateProductStatuses', cron: '0 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '0 */3 * * *' },
     ],
   },
   skidoc: {
     vendorId: '6dcd1d90-b7b3-4ef9-bc44-f8b7cb545fef',
     synchros: [
       { commandName: 'syncProducts', cron: '0 6 * * *' },
-      { commandName: 'updateProductStatuses', cron: '20 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '20 */3 * * *' },
     ],
   },
   freeride: { vendorId: '5b2bb9d6-c4e4-46eb-8564-0f0b03436543', synchros: [] },
@@ -158,14 +158,14 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: '36fa1a49-2146-4484-bba1-6d883090b86c',
     synchros: [
       { commandName: 'syncProducts', cron: '0 4 * * *' },
-      { commandName: 'updateProductStatuses', cron: '0 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '0 */3 * * *' },
     ],
   },
   mbspro: {
     vendorId: 'a207c89c-e2e1-4e66-bef9-98f983f1446e',
     synchros: [
       { commandName: 'syncProducts', cron: '30 2 * * *' },
-      { commandName: 'updateProductStatuses', cron: '0 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '0 */3 * * *' },
     ],
   },
   used_elite_bikes: {
@@ -179,7 +179,7 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: '91d89f86-da6c-4fcd-a22d-0abb7ad98e0c',
     synchros: [
       { commandName: 'syncProducts', cron: '0 1 * * *' },
-      { commandName: 'updateProductStatuses', cron: '40 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '40 */3 * * *' },
     ],
   },
   sanferbike: {
@@ -190,28 +190,28 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: 'd99c1c6d-98e9-4cca-a758-f1c3d06a8eec',
     synchros: [
       { commandName: 'syncProducts', cron: '30 3 * * *' },
-      { commandName: 'updateProductStatuses', cron: '0 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '0 */3 * * *' },
     ],
   },
   club_in_sport: {
     vendorId: '7e6b6a67-27e2-4438-8929-2b1815e20271',
     synchros: [
       { commandName: 'syncProducts', cron: '30 6 * * *' },
-      { commandName: 'updateProductStatuses', cron: '30 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '30 */3 * * *' },
     ],
   },
   bernaudeau_woo: {
     vendorId: '3cbb2030-a8ca-4bb4-a6c7-e883b2e13169',
     synchros: [
       { commandName: 'syncProducts', cron: '0 23 * * *' },
-      { commandName: 'updateProductStatuses', cron: '50 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '50 */3 * * *' },
     ],
   },
   ciklet: {
     vendorId: '938e2c7b-e692-4443-a87c-00fb874c9d5f',
     synchros: [
       { commandName: 'syncProducts', cron: '0 0 * * *' },
-      { commandName: 'updateProductStatuses', cron: '40 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '40 */2 * * *' },
     ],
   },
   montanini: {
@@ -226,21 +226,21 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: '205ad821-fd08-4cb3-8e76-78af089e0006',
     synchros: [
       { commandName: 'syncProducts', cron: '0 4 * * *' },
-      { commandName: 'updateProductStatuses', cron: '40 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '40 */3 * * *' },
     ],
   },
   velosport34: {
     vendorId: 'cd7a5a5e-3cb8-4ce9-886e-f14ca1424fd5',
     synchros: [
       { commandName: 'syncProducts', cron: '30 3 * * *' },
-      { commandName: 'updateProductStatuses', cron: '10 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '10 */3 * * *' },
     ],
   },
   daz_bike: {
     vendorId: '0a61c4e1-cdcd-40b7-88a3-71d0062d6d4e',
     synchros: [
       { commandName: 'syncProducts', cron: '30 23 * * *' },
-      { commandName: 'updateProductStatuses', cron: '20 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '20 */3 * * *' },
     ],
   },
   pilat: {
@@ -254,14 +254,14 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: 'f89cec51-7968-41d6-b42b-a967928a9591',
     synchros: [
       { commandName: 'syncProducts', cron: '0 23 * * *' },
-      { commandName: 'updateProductStatuses', cron: '20 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '20 */3 * * *' },
     ],
   },
   zyclora: {
     vendorId: '37c4f291-36d9-40de-933e-719eb4d2d833',
     synchros: [
       { commandName: 'syncProducts', cron: '0 23 * * *' },
-      { commandName: 'updateProductStatuses', cron: '0 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '0 */2 * * *' },
     ],
   },
   le_bon_coin: {
@@ -286,7 +286,7 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: '92d87ca9-efd5-4468-b7b3-16d9a56cfe20',
     synchros: [
       { commandName: 'syncProducts', cron: '30 23 * * *' },
-      { commandName: 'updateProductStatuses', cron: '30 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '30 */3 * * *' },
     ],
   },
   chris_bikes: {
@@ -319,14 +319,14 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: 'ab563cee-ad89-4953-a958-977c1621a043',
     synchros: [
       { commandName: 'syncProducts', cron: '30 1 * * *' },
-      { commandName: 'updateProductStatuses', cron: '0 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '0 */3 * * *' },
     ],
   },
   milla_bikes: {
     vendorId: '3f50f794-44c2-4095-831a-f1f70f58a67a',
     synchros: [
       { commandName: 'syncProducts', cron: '0 4 * * *' },
-      { commandName: 'updateProductStatuses', cron: '0 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '0 */3 * * *' },
     ],
   },
   willemd: {
@@ -358,7 +358,7 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: '7e5c2537-8fc2-400e-90b2-3c66a864ca7d',
     synchros: [
       { commandName: 'syncProducts', cron: '0 1 * * *' },
-      { commandName: 'updateProductStatuses', cron: '0 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '0 */3 * * *' },
     ],
   },
   velo_meldois: {
@@ -393,7 +393,7 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: '3cfd67a4-5aea-40b9-99ab-790d15cbaa08',
     synchros: [
       { commandName: 'syncProducts', cron: '0 4 * * *' },
-      { commandName: 'updateProductStatuses', cron: '30 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '30 */3 * * *' },
     ],
   },
   alpin_store_orders: {
@@ -404,14 +404,14 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: '9fa2866d-ad2d-4ab8-b0f3-9de1e1e6f41e',
     synchros: [
       { commandName: 'syncProducts', cron: '0 5 * * *' },
-      { commandName: 'updateProductStatuses', cron: '50 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '50 */3 * * *' },
     ],
   },
   agava_parts: {
     vendorId: '9fa2866d-ad2d-4ab8-b0f3-9de1e1e6f41e',
     synchros: [
       { commandName: 'syncProducts', cron: '0 21 * * *' },
-      { commandName: 'updateProductStatuses', cron: '40 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '40 */3 * * *' },
     ],
   },
   mvh_cycles: {
@@ -422,7 +422,7 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: '2d1e8cea-9183-4cf6-b3d4-4f0e3f1c5ab1',
     synchros: [
       { commandName: 'syncProducts', cron: '0 4 * * *' },
-      { commandName: 'updateProductStatuses', cron: '50 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '50 */3 * * *' },
     ],
   },
   darosa_bikes: {
@@ -447,7 +447,7 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: '67b93b6f-7467-402d-8f32-91a2ebb8f9ea',
     synchros: [
       { commandName: 'syncProducts', cron: '0 9 * * *' },
-      { commandName: 'updateProductStatuses', cron: '30 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '30 */3 * * *' },
     ],
   },
   fastlap: {
@@ -461,7 +461,7 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: 'a485ddbe-2001-43ee-9c98-dfb00fc1c9b0',
     synchros: [
       { commandName: 'syncProducts', cron: '0 0 * * *' },
-      { commandName: 'updateProductStatuses', cron: '20 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '20 */3 * * *' },
     ],
   },
   hbe_shopify: {
@@ -472,7 +472,7 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: '28553b01-476c-4afc-b412-8faf57fa428d',
     synchros: [
       { commandName: 'syncProducts', cron: '0 16 * * *' },
-      { commandName: 'updateProductStatuses', cron: '10 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '10 */3 * * *' },
     ],
   },
   sbikes: {
@@ -487,28 +487,28 @@ export const prodVendorConfig: EnvVendorsConfig = {
     vendorId: 'efaea235-d824-484e-b3a7-6df24e89ad33',
     synchros: [
       { commandName: 'syncProducts', cron: '0 12 * * *' },
-      { commandName: 'updateProductStatuses', cron: '10 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '10 */3 * * *' },
     ],
   },
   manufaktur: {
     vendorId: '5487023d-4b3b-40d3-92c3-2e9cad71b388',
     synchros: [
       { commandName: 'syncProducts', cron: '30 22 * * *' },
-      { commandName: 'updateProductStatuses', cron: '30 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '30 */3 * * *' },
     ],
   },
   elettronic: {
     vendorId: '21a9373d-904f-4843-9701-6d9573040a24',
     synchros: [
       { commandName: 'syncProducts', cron: '0 12 * * *' },
-      { commandName: 'updateProductStatuses', cron: '20 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '20 */3 * * *' },
     ],
   },
   moulin_a_velos: {
     vendorId: 'b4a5518e-f59f-4999-8ea6-84fafaa91ccd',
     synchros: [
       { commandName: 'syncProducts', cron: '0 17 * * *' },
-      { commandName: 'updateProductStatuses', cron: '10 * * * *' },
+      { commandName: 'updateProductStatuses', cron: '10 */3 * * *' },
     ],
   },
 };
