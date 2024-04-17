5a2b5896856ea663dabce0d43d23db44b87236b0
jeanphilippeds <jean-philippe@barooders.com>
2024-04-17 09:07:42 +0200

    :recycle: use new chatId to fetch chat data

--- packages/frontend/shared/components/pages/Chat/index.tsx
+++ packages/frontend/shared/components/pages/Chat/index.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import { FetchCustomerQuery } from '@/__generated/graphql';
 import Modal from '@/components/atoms/Modal';
 import PageContainer from '@/components/atoms/PageContainer';
-import { useHasuraToken } from '@/hooks/useHasuraToken';
+import { useHasura } from '@/hooks/useHasura';
+import useWrappedAsyncFn from '@/hooks/useWrappedAsyncFn';
 import { getDictionary } from '@/i18n/translate';
+import { gql } from '@apollo/client';
+import { head } from 'lodash';
+import { useEffect } from 'react';
 import { FaCircleExclamation } from 'react-icons/fa6';
+import { HASURA_ROLES } from 'shared-types';
 import Inbox from './_components/Inbox';
 
 const dict = getDictionary('fr');
@@ -14,11 +20,35 @@ type PropsType = {
   selectedConversationId?: string;
 };
 
+const GET_CUSTOMER = gql`
+  query fetchCustomer {
+    Customer {
+      chatId
+      sellerName
+    }
+  }
+`;
+
 const Chat: React.FC<PropsType> = ({ productId, selectedConversationId }) => {
-  const { extractTokenInfo } = useHasuraToken();
-  const tokenInfo = extractTokenInfo();
-  const customerId = String(tokenInfo.shopifyId);
-  const customerName = String(tokenInfo.sellerName) ?? '';
+  const fetchCustomer = useHasura<FetchCustomerQuery>(
+    GET_CUSTOMER,
+    HASURA_ROLES.ME_AS_CUSTOMER,
+  );
+  const [customerState, doFetchCustomer] = useWrappedAsyncFn(
+    () => fetchCustomer(),
+    [fetchCustomer],
+  );
+
+  useEffect(() => {
+    (async () => {
+      doFetchCustomer();
+    })();
+    doFetchCustomer();
+  }, [doFetchCustomer]);
+
+  const customer = head(customerState.value?.Customer);
+
+  if (!customer?.chatId) return <></>;
 
   return (
     <PageContainer>
@@ -40,8 +70,8 @@ const Chat: React.FC<PropsType> = ({ productId, selectedConversationId }) => {
         />
       </div>
       <Inbox
-        customerId={customerId}
-        customerName={customerName}
+        customerId={customer.chatId}
+        customerName={customer.sellerName ?? ''}
         productId={productId}
         initialConversationId={selectedConversationId}
       />
