b13312f03c3e2be9b9da7c9a0b36384eaa52da78
jeanphilippeds <jean-philippe@barooders.com>
2024-04-24 22:41:07 +0200

    :green_heart: fix deploy

--- packages/backend/prisma/migrations/20240424203016_add_new_columns/migration.sql
+++ packages/backend/prisma/migrations/20240424203016_add_new_columns/migration.sql
@@ -8,4 +8,4 @@
 ALTER TABLE "dbt"."store_b2c_product_variant" ADD COLUMN     "compare_at_price" DOUBLE PRECISION;
 
 -- AlterTable
-ALTER TABLE "dbt"."store_exposed_product_variant" ADD COLUMN     "inventory_quantity" BIGINT NOT NULL;
+ALTER TABLE "dbt"."store_exposed_product_variant" ADD COLUMN     "inventory_quantity" BIGINT NOT NULL DEFAULT 0;
