7934313fa0328af19e6522c40312939759373d88
jeanphilippeds <jean-philippe@barooders.com>
2024-04-24 14:49:17 +0200

    :recycle: refactor saved search results

--- packages/backend/src/modules/search-alert/domain/ports/search-repository.ts
+++ packages/backend/src/modules/search-alert/domain/ports/search-repository.ts
@@ -1,19 +1,24 @@
+import { SavedSearchType } from '@libs/domain/prisma.main.client';
+
+export type SearchResults = {
+  nbHits: number;
+  hits: {
+    imageUrl: string;
+    brand: string;
+    handle: string;
+    characteristics: string;
+    price: string;
+    compareAtPrice: string;
+    discount: string;
+  }[];
+};
+
 export abstract class SearchRepository {
   abstract getSavedSearchResults(
+    type: SavedSearchType,
     query: string,
     facetFilters: { facetName: string; value: string }[],
     numericFilters: { facetName: string; value: string; operator: string }[],
     since: Date,
-  ): Promise<{
-    nbHits: number;
-    hits: {
-      imageUrl: string;
-      brand: string;
-      handle: string;
-      characteristics: string;
-      price: string;
-      compareAtPrice: string;
-      discount: string;
-    }[];
-  }>;
+  ): Promise<SearchResults>;
 }
--- packages/backend/src/modules/search-alert/infrastructure/search/typesense.repository.ts
+++ packages/backend/src/modules/search-alert/infrastructure/search/typesense.repository.ts
@@ -1,10 +1,14 @@
+import { SavedSearchType } from '@libs/domain/prisma.main.client';
 import { Condition } from '@libs/domain/prisma.store.client';
 import { jsonStringify } from '@libs/helpers/json';
 import {
   typesensePublicVariantClient,
   TypesensePublicVariantDocument,
 } from '@libs/infrastructure/typesense/typesense.base.client';
-import { SearchRepository } from '@modules/search-alert/domain/ports/search-repository';
+import {
+  SearchRepository,
+  SearchResults,
+} from '@modules/search-alert/domain/ports/search-repository';
 import { Logger } from '@nestjs/common';
 import { head } from 'lodash';
 import { SearchResponseHit } from 'typesense/lib/Typesense/Documents';
@@ -13,22 +17,12 @@ export class TypesenseRepository implements SearchRepository {
   private readonly logger = new Logger(TypesenseRepository.name);
 
   async getSavedSearchResults(
+    type: SavedSearchType,
     query: string,
     facetFilters: { facetName: string; value: string }[],
     numericFilters: { facetName: string; value: string; operator: string }[],
     since: Date,
-  ): Promise<{
-    nbHits: number;
-    hits: {
-      imageUrl: string;
-      brand: string;
-      handle: string;
-      characteristics: string;
-      price: string;
-      compareAtPrice: string;
-      discount: string;
-    }[];
-  }> {
+  ): Promise<SearchResults> {
     const queryOptions = {
       facetFilters: facetFilters.map(
         (facetFilter) => `${facetFilter.facetName}:=${facetFilter.value}`,
@@ -47,6 +41,22 @@ export class TypesenseRepository implements SearchRepository {
       `Searching with options ${jsonStringify(queryOptions, 2)}`,
     );
 
+    if (type === SavedSearchType.PUBLIC_COLLECTION_PAGE)
+      return this.getPublicSavedSearchResults({ query, queryOptions });
+
+    throw new Error(`Unsupported search type: ${type}`);
+  }
+
+  private async getPublicSavedSearchResults({
+    query,
+    queryOptions,
+  }: {
+    query: string;
+    queryOptions: {
+      facetFilters: string[];
+      numericFilters: string[];
+    };
+  }): Promise<SearchResults> {
     const { hits, found: nbHits } = await typesensePublicVariantClient
       .documents()
       .search({
