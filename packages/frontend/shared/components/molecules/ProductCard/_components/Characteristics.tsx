f42ca2f99f5f1bd65f9f05a0c97e94686b060566
Nicolas Ngomai <nicolas.ngomai@gmail.com>
2024-04-25 18:49:26 +0200

    💄 Add wrap on characteristics

--- packages/frontend/shared/components/molecules/ProductCard/_components/Characteristics.tsx
+++ packages/frontend/shared/components/molecules/ProductCard/_components/Characteristics.tsx
@@ -66,7 +66,7 @@ const Characteristics: React.FC<{
         {brand}{' '}
       </p>
       <p
-        className={`flex gap-1 tracking-tight text-gray-600 ${
+        className={`flex flex-wrap gap-1 tracking-tight text-gray-600 ${
           componentSize === 'large' ? 'text-sm' : 'text-xs lg:text-sm'
         }`}
       >
