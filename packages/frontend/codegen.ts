2b559b602512356510445d5092e8ac36b058d290
jeanphilippeds <jean-philippe@barooders.com>
2024-04-27 17:40:32 +0200

    :sparkles: create codegen project for admin types

--- packages/frontend/codegen.ts
+++ packages/frontend/codegen.ts
@@ -1,38 +1,47 @@
 import dotenv from 'dotenv';
+import { IGraphQLConfig } from 'graphql-config';
 
 dotenv.config();
 
 const getHasuraSecretKey = () =>
   process.env.HASURA_SECRET_KEY ?? 'myadminsecretkey';
 
-module.exports = {
-  schema: {
-    [process.env.NEXT_PUBLIC_HASURA_BASE_URL]: {
-      headers: {
-        'X-Hasura-Admin-Secret': getHasuraSecretKey(),
+const config: IGraphQLConfig = {
+  projects: {
+    admin: {
+      schema: {
+        [process.env.NEXT_PUBLIC_HASURA_BASE_URL]: {
+          headers: {
+            'X-Hasura-Admin-Secret': getHasuraSecretKey(),
+            'X-Hasura-Role': 'admin',
+          },
+        },
       },
-    },
-  },
-  documents: [
-    './shared/**/*.tsx',
-    './shared/**/*.ts',
-    './web/**/*.tsx',
-    './web/**/*.ts',
-    '!./shared/hooks/useStorefront.ts',
-  ],
-  overwrite: true,
-  generates: {
-    './shared/__generated/graphql.tsx': {
-      plugins: ['typescript', 'typescript-operations'],
-      config: {
-        avoidOptionals: true,
-        skipTypename: false,
+      documents: [
+        './shared/**/*.tsx',
+        './shared/**/*.ts',
+        './web/**/*.tsx',
+        './web/**/*.ts',
+        '!./shared/hooks/useStorefront.ts',
+      ],
+      extensions: {
+        codegen: {
+          generates: {
+            './shared/__generated/graphql.admin.tsx': {
+              plugins: ['typescript', 'typescript-operations'],
+              config: {
+                avoidOptionals: true,
+                skipTypename: false,
+              },
+            },
+            './shared/__generated/graphql.admin.schema.json': {
+              plugins: ['introspection'],
+            },
+          },
+        },
       },
-    },
-    './shared/__generated/graphql.schema.json': {
-      plugins: ['introspection'],
     },
   },
 };
 
-export {};
+export default config;
