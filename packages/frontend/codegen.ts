a97f91922ee88e4fb8c92adaa6a20cb490c1e13b
jeanphilippeds <jean-philippe@barooders.com>
2024-04-28 18:54:13 +0200

    :recycle: generate types programmatically

--- packages/frontend/codegen.ts
+++ packages/frontend/codegen.ts
@@ -1,47 +1,44 @@
+import { generate } from '@graphql-codegen/cli';
 import dotenv from 'dotenv';
-import { IGraphQLConfig } from 'graphql-config';
 
 dotenv.config();
 
 const getHasuraSecretKey = () =>
   process.env.HASURA_SECRET_KEY ?? 'myadminsecretkey';
 
-const config: IGraphQLConfig = {
-  projects: {
-    admin: {
-      schema: {
-        [process.env.NEXT_PUBLIC_HASURA_BASE_URL]: {
-          headers: {
-            'X-Hasura-Admin-Secret': getHasuraSecretKey(),
-            'X-Hasura-Role': 'admin',
-          },
-        },
+if (!process.env.NEXT_PUBLIC_HASURA_BASE_URL)
+  throw new Error('Missing HASURA_BASE_URL');
+
+const config = {
+  schema: {
+    [process.env.NEXT_PUBLIC_HASURA_BASE_URL]: {
+      headers: {
+        'X-Hasura-Admin-Secret': getHasuraSecretKey(),
+        'X-Hasura-Role': 'admin',
       },
-      documents: [
-        './shared/**/*.tsx',
-        './shared/**/*.ts',
-        './web/**/*.tsx',
-        './web/**/*.ts',
-        '!./shared/hooks/useStorefront.ts',
-      ],
-      extensions: {
-        codegen: {
-          generates: {
-            './shared/__generated/graphql.admin.tsx': {
-              plugins: ['typescript', 'typescript-operations'],
-              config: {
-                avoidOptionals: true,
-                skipTypename: false,
-              },
-            },
-            './shared/__generated/graphql.admin.schema.json': {
-              plugins: ['introspection'],
-            },
-          },
-        },
+    },
+  },
+  documents: [
+    './shared/**/*.tsx',
+    './shared/**/*.ts',
+    './web/**/*.tsx',
+    './web/**/*.ts',
+    '!./shared/hooks/useStorefront.ts',
+  ],
+  generates: {
+    './shared/__generated/graphql.admin.tsx': {
+      plugins: ['typescript', 'typescript-operations'],
+      config: {
+        avoidOptionals: true,
+        skipTypename: false,
       },
+    },
+    './shared/__generated/graphql.admin.schema.json': {
+      plugins: ['introspection'],
     },
   },
 };
 
-export default config;
+(async () => {
+  await generate(config, true);
+})();
