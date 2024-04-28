2c55c1ddc77ca7f2015905e71048292f8d57911d
jeanphilippeds <jean-philippe@barooders.com>
2024-04-28 22:18:43 +0200

    :technologist: loop on all roles

--- packages/frontend/codegen.ts
+++ packages/frontend/codegen.ts
@@ -1,44 +1,49 @@
 import { generate } from '@graphql-codegen/cli';
 import dotenv from 'dotenv';
+import { HASURA_ROLES } from 'shared-types';
 
 dotenv.config();
 
 const getHasuraSecretKey = () =>
   process.env.HASURA_SECRET_KEY ?? 'myadminsecretkey';
 
-if (!process.env.NEXT_PUBLIC_HASURA_BASE_URL)
-  throw new Error('Missing HASURA_BASE_URL');
+const getConfigFromRole = (role: HASURA_ROLES) => {
+  if (!process.env.NEXT_PUBLIC_HASURA_BASE_URL)
+    throw new Error('Missing HASURA_BASE_URL');
 
-const config = {
-  schema: {
-    [process.env.NEXT_PUBLIC_HASURA_BASE_URL]: {
-      headers: {
-        'X-Hasura-Admin-Secret': getHasuraSecretKey(),
-        'X-Hasura-Role': 'admin',
+  return {
+    schema: {
+      [process.env.NEXT_PUBLIC_HASURA_BASE_URL]: {
+        headers: {
+          'X-Hasura-Admin-Secret': getHasuraSecretKey(),
+          'X-Hasura-Role': role,
+        },
       },
     },
-  },
-  documents: [
-    './shared/**/*.tsx',
-    './shared/**/*.ts',
-    './web/**/*.tsx',
-    './web/**/*.ts',
-    '!./shared/hooks/useStorefront.ts',
-  ],
-  generates: {
-    './shared/__generated/graphql.admin.tsx': {
-      plugins: ['typescript', 'typescript-operations'],
-      config: {
-        avoidOptionals: true,
-        skipTypename: false,
+    documents: [
+      './shared/**/*.tsx',
+      './shared/**/*.ts',
+      './web/**/*.tsx',
+      './web/**/*.ts',
+      '!./shared/hooks/useStorefront.ts',
+    ],
+    generates: {
+      [`./shared/__generated/graphql.${role}.tsx`]: {
+        plugins: ['typescript', 'typescript-operations'],
+        config: {
+          avoidOptionals: true,
+          skipTypename: false,
+        },
+      },
+      [`./shared/__generated/graphql.${role}.schema.json`]: {
+        plugins: ['introspection'],
       },
     },
-    './shared/__generated/graphql.admin.schema.json': {
-      plugins: ['introspection'],
-    },
-  },
+  };
 };
 
 (async () => {
-  await generate(config, true);
+  Object.values(HASURA_ROLES).forEach(async (role) => {
+    await generate(getConfigFromRole(role), true);
+  });
 })();
