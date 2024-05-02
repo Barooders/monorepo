8f5802552d091d6e509c3316ec8f5ab9fa0a92e4
Clement Dessoude <clement@barooders.com>
2024-05-02 13:12:06 +0200

    ✨ Add cross to remove request

--- packages/frontend/shared/components/molecules/B2BClientRequestButton/B2BCLientRequestItemForm.tsx
+++ packages/frontend/shared/components/molecules/B2BClientRequestButton/B2BCLientRequestItemForm.tsx
@@ -1,11 +1,13 @@
 import Input from '@/components/molecules/FormInput';
 import FormTextArea from '@/components/molecules/FormTextArea';
 import { getDictionary } from '@/i18n/translate';
+import { ImCross } from 'react-icons/im';
 
 const dict = getDictionary('fr');
 
 type PropsType = {
   index: number;
+  remove: () => void;
 };
 
 export type B2BClientRequestItemFormInputs = {
@@ -18,11 +20,22 @@ export type B2BClientRequestItemFormInputs = {
 
 const FIELD_ARRAY_NAME = 'requests';
 
-const B2BClientRequestItemForm: React.FC<PropsType> = ({ index }) => {
+const B2BClientRequestItemForm: React.FC<PropsType> = ({ index, remove }) => {
   return (
     <div className="mb-4 flex flex-col gap-2 rounded-lg border border-gray-300 px-3 py-2">
-      <div className="text-base font-semibold">
-        {dict.b2b.proPage.clientRequests.form.need} {index + 1}
+      <div className="flex justify-between">
+        <span className="text-base font-semibold">
+          {dict.b2b.proPage.clientRequests.form.need} {index + 1}
+        </span>
+        {index !== 0 ? (
+          <ImCross
+            size={25}
+            className='hover:text-gray-900" cursor-pointer rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200'
+            onClick={remove}
+          />
+        ) : (
+          <></>
+        )}
       </div>
       <Input
         label={dict.b2b.proPage.clientRequests.form.unitCount}
