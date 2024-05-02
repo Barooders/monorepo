7f176fa22508d20f9b23bcfacd6ba1c348a5d3a9
Clement Dessoude <clement@barooders.com>
2024-05-02 09:38:33 +0200

    ♻️ rename ol to order_line

--- packages/data-pipeline/models/analytics/datasets/orders/fact_order_line_refund.sql
+++ packages/data-pipeline/models/analytics/datasets/orders/fact_order_line_refund.sql
@@ -8,14 +8,14 @@ with fact_order_line_refund as (
         olr.order_line_id,
         refund.order_id,
         olr.quantity as refund_quantity,
-        ol.quantity as order_line_quantity,
-        ol.product_price as order_line_product_price,
-        ol.total_discounts as order_line_discounts,
-        ol.shipping_amount as order_line_shipping_amount,
-        ol.quantity * ol.product_price as order_line_good_values,
-        ol.owner as owner,
+        order_line.quantity as order_line_quantity,
+        order_line.product_price as order_line_product_price,
+        order_line.total_discounts as order_line_discounts,
+        order_line.shipping_amount as order_line_shipping_amount,
+        order_line.quantity * order_line.product_price as order_line_good_values,
+        order_line.owner as owner,
         case
-            when ol.id is not null then ol.total_price
+            when order_line.id is not null then order_line.total_price
             else t.amount
         end as order_total_amount,
         case
@@ -24,7 +24,7 @@ with fact_order_line_refund as (
         end as refund_total_amount
     from shopify.refund refund
     left join shopify.order_line_refund olr on olr.refund_id = refund.id
-    left join {{ref('fact_order_line')}} ol on ol.id = olr.order_line_id
+    left join {{ref('fact_order_line')}} order_line on order_line.id = olr.order_line_id
     left join shopify.`transaction` t on t.refund_id = refund.id
 
 )
