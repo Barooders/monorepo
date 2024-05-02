ab8afb1db99bbf7f3950c4c6ee74bae0573f422c
Clement Dessoude <clement@barooders.com>
2024-05-02 09:38:01 +0200

    ♻️ rename r to refund

--- packages/data-pipeline/models/analytics/datasets/orders/fact_order_line_refund.sql
+++ packages/data-pipeline/models/analytics/datasets/orders/fact_order_line_refund.sql
@@ -1,13 +1,12 @@
 {{ config(materialized='table') }}
 
 with fact_order_line_refund as (
-    
-    select 
-        r.id ,
-        DATETIME(r.created_at, 'Europe/Paris') as creation_datetime,
-        date_trunc(DATETIME(r.created_at, 'Europe/Paris'), day) as creation_date,
+    select
+        refund.id ,
+        DATETIME(refund.created_at, 'Europe/Paris') as creation_datetime,
+        date_trunc(DATETIME(refund.created_at, 'Europe/Paris'), day) as creation_date,
         olr.order_line_id,
-        r.order_id,
+        refund.order_id,
         olr.quantity as refund_quantity,
         ol.quantity as order_line_quantity,
         ol.product_price as order_line_product_price,
@@ -15,18 +14,18 @@ with fact_order_line_refund as (
         ol.shipping_amount as order_line_shipping_amount,
         ol.quantity * ol.product_price as order_line_good_values,
         ol.owner as owner,
-        case 
-            when ol.id is not null then ol.total_price 
-            else t.amount 
+        case
+            when ol.id is not null then ol.total_price
+            else t.amount
         end as order_total_amount,
-        case 
-            when olr.id is not null then olr.subtotal 
-            else t.amount 
+        case
+            when olr.id is not null then olr.subtotal
+            else t.amount
         end as refund_total_amount
-    from shopify.refund r
-    left join shopify.order_line_refund olr on olr.refund_id = r.id
+    from shopify.refund refund
+    left join shopify.order_line_refund olr on olr.refund_id = refund.id
     left join {{ref('fact_order_line')}} ol on ol.id = olr.order_line_id
-    left join shopify.`transaction` t on t.refund_id = r.id 
+    left join shopify.`transaction` t on t.refund_id = refund.id
 
 )
 
