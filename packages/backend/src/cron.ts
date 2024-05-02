98c9849ce0e3ff50e87e5269c56a571ee31d42ec
jeanphilippeds <jean-philippe@barooders.com>
2024-05-02 12:09:40 +0200

    :recycle: refactor run job

--- packages/backend/src/cron.ts
+++ packages/backend/src/cron.ts
@@ -2,39 +2,15 @@
 import 'dotenv.config';
 
 import envConfig from '@config/env/env.config';
-import { jsonStringify } from '@libs/helpers/json';
+import { runJob } from '@libs/infrastructure/render/run-job';
 import { schedule } from 'node-cron';
 
-const {
-  cronJobs,
-  externalServices: {
-    commandHandler: { bearerToken, endpoint },
-  },
-} = envConfig;
+const { cronJobs, envName } = envConfig;
 
 console.log(`Starting ${cronJobs.length} cron jobs`);
 
 cronJobs.forEach(({ command, cron }) => {
   schedule(cron, async () => {
-    try {
-      const result = await fetch(endpoint, {
-        method: 'POST',
-        headers: new Headers([
-          ['Content-Type', 'application/json'],
-          ['Authorization', `Bearer ${bearerToken}`],
-        ]),
-        body: jsonStringify({ startCommand: command }),
-      });
-
-      if (!result.ok) {
-        console.error(
-          `Failed create job with command: ${command}. Status: ${result.status}`,
-        );
-      }
-
-      console.log(`Running command: ${command} as requested by: ${cron}`);
-    } catch (error) {
-      console.error(`An unexpected error occured:`, error);
-    }
+    await runJob(command, envName, cron);
   });
 });
