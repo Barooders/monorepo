1af8ec0b1f9820fbc47c918de496cb5c95cc5a2f
Clement Dessoude <clement@barooders.com>
2024-05-02 11:07:49 +0200

    🐛 fix data pipeline dbt by modifying reserved keyword

--- packages/data-pipeline/models/analytics/datasets/orders/fact_order.sql
+++ packages/data-pipeline/models/analytics/datasets/orders/fact_order.sql
@@ -3,21 +3,21 @@
 
 with fact_order as (
     select
-        order.id,
-        order.name as order_name,
-        order.createdat as creation_datetime,
-        date_trunc(order.createdat, day) as creation_date,
-        order.shopifyid as shopify_id,
-        order.customerid as customer_id,
+        backend_order.id,
+        backend_order.name as order_name,
+        backend_order.createdat as creation_datetime,
+        date_trunc(backend_order.createdat, day) as creation_date,
+        backend_order.shopifyid as shopify_id,
+        backend_order.customerid as customer_id,
         customer.shopifyid as customer_shopify_id,
-        order.customeremail as customer_email,
-        order.shippingaddressfirstname as shipping_address_first_name,
-        order.shippingaddresslastname as shipping_address_last_name,
-        order.shippingaddresszip,
-        order.shippingaddresscity,
-        order.shippingaddressphone,
-        order.status as status,
-        from barooders_backend_public.order order
+        backend_order.customeremail as customer_email,
+        backend_order.shippingaddressfirstname as shipping_address_first_name,
+        backend_order.shippingaddresslastname as shipping_address_last_name,
+        backend_order.shippingaddresszip,
+        backend_order.shippingaddresscity,
+        backend_order.shippingaddressphone,
+        backend_order.status as status,
+        from barooders_backend_public.order backend_order
         join barooders_backend_public.customer customer on customer.authuserid = order.customerid
 )
 
