352189249cda084a60bbc189ae9d8a24e6b3537d
Clement Dessoude <clement@barooders.com>
2024-05-02 09:37:07 +0200

    ♻️ rename variables

--- packages/data-pipeline/models/analytics/datasets/orders/fact_order.sql
+++ packages/data-pipeline/models/analytics/datasets/orders/fact_order.sql
@@ -3,22 +3,22 @@
 
 with fact_order as (
     select
-        o.id,
-        o.name as order_name,
-        o.createdat as creation_datetime,
-        date_trunc(o.createdat, day) as creation_date,
-        o.shopifyid as shopify_id,
-        o.customerid as customer_id,
-        c.shopifyid as customer_shopify_id,
-        o.customeremail as customer_email,
-        o.shippingaddressfirstname as shipping_address_first_name,
-        o.shippingaddresslastname as shipping_address_last_name,
-        o.shippingaddresszip,
-        o.shippingaddresscity,
-        o.shippingaddressphone,
-        o.status as status,
-        from barooders_backend_public.order o
-        join barooders_backend_public.customer c on c.authuserid = o.customerid
+        order.id,
+        order.name as order_name,
+        order.createdat as creation_datetime,
+        date_trunc(order.createdat, day) as creation_date,
+        order.shopifyid as shopify_id,
+        order.customerid as customer_id,
+        customer.shopifyid as customer_shopify_id,
+        order.customeremail as customer_email,
+        order.shippingaddressfirstname as shipping_address_first_name,
+        order.shippingaddresslastname as shipping_address_last_name,
+        order.shippingaddresszip,
+        order.shippingaddresscity,
+        order.shippingaddressphone,
+        order.status as status,
+        from barooders_backend_public.order order
+        join barooders_backend_public.customer customer on customer.authuserid = order.customerid
 )
 
 select *
