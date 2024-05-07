0aa9cf17b272bef69596d00c33a9e33e5afb70de
Clement Dessoude <clement@barooders.com>
2024-05-07 15:45:41 +0200

    ✨ Add B2B menu

--- packages/frontend/shared/components/layouts/pro.tsx
+++ packages/frontend/shared/components/layouts/pro.tsx
@@ -1,5 +1,6 @@
 import { metadataConfig, viewportConfig } from '@/document/metadata/global';
 import B2BHeader from '../molecules/B2BHeader';
+import { MegaMenuChunk } from '../molecules/MegaMenu/shared/types/app/MegaMenu.types';
 import BaseLayout from './base';
 
 export const metadata = metadataConfig;
@@ -7,10 +8,11 @@ export const viewport = viewportConfig;
 
 export type PropsType = {
   children?: React.ReactNode;
+  menu: MegaMenuChunk;
 };
 
-const ProLayout: React.FC<PropsType> = ({ children }) => {
-  return <BaseLayout header={<B2BHeader />}>{children}</BaseLayout>;
+const ProLayout: React.FC<PropsType> = ({ children, menu }) => {
+  return <BaseLayout header={<B2BHeader menu={menu} />}>{children}</BaseLayout>;
 };
 
 export default ProLayout;
--- packages/frontend/web/src/app/(pro-layout)/layout.tsx
+++ packages/frontend/web/src/app/(pro-layout)/layout.tsx
@@ -1,4 +1,5 @@
 import ProLayout from '@/components/layouts/pro';
+import { getB2BMenuData } from '@/components/molecules/B2BMenu';
 import { poppins } from '@/document/fonts';
 import { metadataConfig, viewportConfig } from '@/document/metadata/global';
 import '@/document/styles/globals.scss';
@@ -10,10 +11,12 @@ export const metadata = metadataConfig;
 export const viewport = viewportConfig;
 
 const ProLayoutWeb = async ({ children }: { children?: React.ReactNode }) => {
+  const menu = await getB2BMenuData();
+
   return (
     <html lang={dict.global.locale}>
       <body className={poppins.className}>
-        <ProLayout>{children}</ProLayout>
+        <ProLayout menu={menu}>{children}</ProLayout>
       </body>
     </html>
   );
