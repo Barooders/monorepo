34d1e78a7d6d52ec843607bbbc272672051dc694
Clement Dessoude <clement@barooders.com>
2024-05-10 11:14:02 +0200

    ✨ add redirection to pro search page when submitting search form

--- packages/frontend/web/src/app/(pro-layout)/(authenticated)/pro/page.tsx
+++ packages/frontend/web/src/app/(pro-layout)/(authenticated)/pro/page.tsx
@@ -2,11 +2,18 @@
 
 import ProPage, { PRODUCT_ID_QUERY_KEY } from '@/components/pages/ProPage';
 import useSearchParams from '@/hooks/useSearchParams';
+import { SEARCH_BAR_QUERY_KEY } from '../../../../../../shared/components/molecules/B2BSearchBar';
 
 const WebProPage: React.FC = () => {
   const productInternalId = useSearchParams(PRODUCT_ID_QUERY_KEY);
+  const searchQuery = useSearchParams(SEARCH_BAR_QUERY_KEY);
 
-  return <ProPage productInternalId={productInternalId} />;
+  return (
+    <ProPage
+      productInternalId={productInternalId}
+      searchQuery={searchQuery}
+    />
+  );
 };
 
 export default WebProPage;
