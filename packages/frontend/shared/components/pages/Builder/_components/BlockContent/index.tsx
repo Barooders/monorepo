50ebb31163ccc5aa594b61de34439b7c92aa33e4
Clement Dessoude <clement@barooders.com>
2024-05-15 16:52:35 +0200

    ✨ Add missing buttons on home page block contents

--- packages/frontend/shared/components/pages/Builder/_components/BlockContent/index.tsx
+++ packages/frontend/shared/components/pages/Builder/_components/BlockContent/index.tsx
@@ -1,23 +1,49 @@
+import Button from '@/components/atoms/Button';
+
 type BlockContentProps = {
   title?: string;
   subtitle?: string;
   children: React.ReactNode;
+  button?: {
+    label: string;
+    link: string;
+  };
 };
 
 const BlockContent: React.FC<BlockContentProps> = ({
   children,
   title,
   subtitle,
+  button,
 }) => {
   return (
     <div className="mt-14 p-0 md:px-5">
       <div className="mx-auto flex max-w-page-content flex-col">
-        {subtitle !== undefined && (
-          <span className="text-2xl font-medium text-gray-400">{subtitle}</span>
-        )}
-        {title !== undefined && (
-          <h2 className="mb-3 text-xl font-semibold">{title}</h2>
-        )}
+        <div className="flex justify-between">
+          <div>
+            {subtitle !== undefined && (
+              <span className="text-2xl font-medium text-gray-400">
+                {subtitle}
+              </span>
+            )}
+            {title !== undefined && (
+              <h2 className="mb-3 text-xl font-semibold">{title}</h2>
+            )}
+          </div>
+          {button !== undefined ? (
+            <Button
+              intent="discrete"
+              href={button.link}
+              className="h-fit w-fit bg-gray-200 text-gray-500"
+              size="small"
+            >
+              {button.label}
+            </Button>
+          ) : (
+            <></>
+          )}
+        </div>
+
         {children}
       </div>
     </div>
