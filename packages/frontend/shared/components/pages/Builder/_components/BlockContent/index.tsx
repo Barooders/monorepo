417ddf927af6171673ffda2ac08e468bac1c72ca
Clement Dessoude <clement@barooders.com>
2024-05-15 16:38:29 +0200

    ✨ Improve homepage design

--- packages/frontend/shared/components/pages/Builder/_components/BlockContent/index.tsx
+++ packages/frontend/shared/components/pages/Builder/_components/BlockContent/index.tsx
@@ -10,14 +10,16 @@ const BlockContent: React.FC<BlockContentProps> = ({
   subtitle,
 }) => {
   return (
-    <div className="mt-14 flex flex-col p-0 md:px-5">
-      {subtitle !== undefined && (
-        <span className="text-2xl font-medium text-gray-400">{subtitle}</span>
-      )}
-      {title !== undefined && (
-        <h2 className="mb-3 text-xl font-semibold">{title}</h2>
-      )}
-      {children}
+    <div className="mt-14 p-0 md:px-5">
+      <div className="mx-auto flex max-w-page-content flex-col">
+        {subtitle !== undefined && (
+          <span className="text-2xl font-medium text-gray-400">{subtitle}</span>
+        )}
+        {title !== undefined && (
+          <h2 className="mb-3 text-xl font-semibold">{title}</h2>
+        )}
+        {children}
+      </div>
     </div>
   );
 };
