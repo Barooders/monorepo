8859b5b72ae6cd97344ffdd121b0e742ac2b3b1e
Clement Dessoude <clement@barooders.com>
2024-05-15 11:47:35 +0200

    ✨ Add MainHeader to homepage

--- packages/frontend/shared/clients/strapi.ts
+++ packages/frontend/shared/clients/strapi.ts
@@ -1,3 +1,8 @@
+import { createGraphQLClient } from './base/graphql';
 import { createRestClient } from './base/rest';
 
 export const fetchStrapi = createRestClient(process.env.NEXT_PUBLIC_STRAPI_URL);
+
+export const fetchStrapiGraphQL = createGraphQLClient(
+  process.env.NEXT_PUBLIC_STRAPI_GRAPHQL_URL,
+);
--- packages/frontend/shared/components/pages/Homepage/index.tsx
+++ packages/frontend/shared/components/pages/Homepage/index.tsx
@@ -1,38 +1,88 @@
-import { builder, BuilderContent } from '@builder.io/sdk';
-import { RenderBuilderContent } from '@/components/pages/Builder';
-import { getDictionary } from '@/i18n/translate';
+import { fetchStrapiGraphQL } from '@/clients/strapi';
 import HomeJsonLd from '@/components/atoms/JsonLd/HomeJsonLd';
 import SnowFall from '@/components/molecules/Snowfall';
+import { getDictionary } from '@/i18n/translate';
+// eslint-disable-next-line no-restricted-imports
+import { gql } from '@apollo/client';
+import MainHeader from '../Builder/_components/MainHeader';
 
 const dict = getDictionary('fr');
 
-builder.init(process.env.NEXT_PUBLIC_BUILDER_IO_API_KEY ?? '');
-
 export type PropsType = {
-  content: BuilderContent;
+  header: {
+    images: {
+      image: string;
+      link: string;
+    }[];
+  };
 };
+
+const FETCH_HOMEPAGE_CONFIG = gql`
+  query {
+    webHome {
+      data {
+        attributes {
+          Header {
+            ... on ComponentMediaImageSection {
+              link
+              image {
+                data {
+                  attributes {
+                    url
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export const getData = async (): Promise<PropsType> => {
-  const content = await builder
-    .get('page', {
-      userAttributes: {
-        urlPath: '/',
-      },
-      prerender: false,
-    })
-    .toPromise();
+  const content = await fetchStrapiGraphQL<
+    {
+      webHome: {
+        data: {
+          attributes: {
+            Header: {
+              link: string;
+              image: {
+                data: {
+                  attributes: {
+                    url: string;
+                  };
+                };
+              };
+            }[];
+          };
+        };
+      };
+    },
+    unknown
+  >(FETCH_HOMEPAGE_CONFIG, {});
 
-  return { content };
+  const images = content.webHome.data.attributes.Header.map((item) => ({
+    image: item.image.data.attributes.url,
+    link: item.link,
+  }));
+
+  return { header: { images } };
 };
 
-const HomePage: React.FC<PropsType> = ({ content }) => {
+const HomePage: React.FC<PropsType> = ({ header }) => {
   return (
     <>
       <div className="z-10">
         <SnowFall />
       </div>
+      <MainHeader
+        mainSlide={header.images[0]}
+        desktopSlides={header.images.slice(1)}
+        mobileSlides={header.images.slice(1)}
+      />
       <h1 className="hidden">{dict.homepage.mainTitle}</h1>
-      <RenderBuilderContent content={content} />
       <HomeJsonLd />
     </>
   );
