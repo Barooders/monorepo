af31cf891a5b365deba9e2bb6e201f794173e41e
Nicolas Ngomai <nicolas.ngomai@gmail.com>
2024-05-17 11:46:47 +0200

    🚚 Move google ads dashboards to direct export

--- packages/data-pipeline/models/analytics/datamarts/acquisition_kpis/shopping_performance.sql
+++ packages/data-pipeline/models/analytics/datamarts/acquisition_kpis/shopping_performance.sql
@@ -1,19 +1,18 @@
 {{ config(materialized='table') }}
 
 SELECT
-date,
-p.vendor,
-cast(product_item_id as string) as id,
-product_type_l_2 as product_type,
-product_type_l_1 as category,
-sum(cost_micros) / 1000000 as cost1J,
-sum(conversions_value) as conv_value1J,
-sum(clicks) as clicks1J,
-sum(impressions) as impressions1J,
-sum(conversions) as conversions1J,
-
-FROM google_ads.shopping_performance_view as shp
-LEFT JOIN {{ ref('dim_product_variant') }}  pv on cast(pv.id as string) = shp.product_item_id
+	shp.segments_date AS date,
+	p.vendor,
+	cast(segments_product_item_id as string) as id,
+	shp.segments_product_type_l3 as product_type,
+	shp.segments_product_type_l2 as category,
+	sum(shp.metrics_cost_micros) / 1000000 as cost1J,
+	sum(shp.metrics_all_conversions_value) as conv_value1J,
+	sum(shp.metrics_clicks) as clicks1J,
+	sum(shp.metrics_impressions) as impressions1J,
+	sum(shp.metrics_all_conversions) as conversions1J
+FROM ads_direct_export.p_ads_ShoppingProductStats_5663401656 as shp
+LEFT JOIN {{ ref('dim_product_variant') }} pv on cast(pv.id as string) = shp.segments_product_item_id
 LEFT JOIN {{ ref('dim_product') }} as p on p.id = pv.product_id
 
 group by vendor, id, product_type, category, date
