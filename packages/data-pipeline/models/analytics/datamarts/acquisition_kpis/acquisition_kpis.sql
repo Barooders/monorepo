44e987e5be101d6b2b7d94c971c124f0ff4769ba
Nicolas Ngomai <nicolas.ngomai@gmail.com>
2024-05-20 18:58:01 +0200

    🎨 Fix code formatting

--- packages/data-pipeline/models/analytics/datamarts/acquisition_kpis/acquisition_kpis.sql
+++ packages/data-pipeline/models/analytics/datamarts/acquisition_kpis/acquisition_kpis.sql
@@ -2,13 +2,11 @@
 
 with acquisition_kpis as (
 
-    select 
-        COALESCE(k.`date` , o.`date`) as date, 
-        COALESCE(k.utm_source, o.utm_source) as utm_source, 
+    select
+        COALESCE(k.`date` , o.`date`) as date,
+        COALESCE(k.utm_source, o.utm_source) as utm_source,
         COALESCE(k.utm_medium, o.utm_medium) as utm_medium,
         COALESCE(k.utm_campaign, o.utm_campaign) as utm_campaign,
-        --COALESCE(k.landing_page, o.landing_page) as landing_page,
-        --COALESCE(k.channel, o.channel ) as channel,
         sum(k.impressions) as impressions,
         sum(k.clicks) as clicks,
         sum(k.platforms_conversions) as platforms_conversions,
@@ -21,41 +19,37 @@ with acquisition_kpis as (
         sum(o.count_paid_orders) as shopify_paid_conversions,
         sum(o.gmv) as gmv,
         sum(o.paid_gmv) paid_gmv
-    from {{ref('cost_traffic_kpis')}} k 
+    from {{ref('cost_traffic_kpis')}} k
     full outer join (
-        select cast(date_trunc(creation_date, day) as date) as date, 
-        utm_source, 
-        utm_medium, 
-        utm_campaign, 
-        --landing_page, 
-        --channel, 
-        sum(total_price) gmv, 
-        sum(case when financial_status != 'pending' then total_price else null end) paid_gmv, 
+        select cast(date_trunc(creation_date, day) as date) as date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        sum(total_price) gmv,
+        sum(case when financial_status != 'pending' then total_price else null end) paid_gmv,
         count(distinct order_id) count_orders,
         count(distinct case when financial_status != 'pending' then order_id else null end) count_paid_orders,
-        from dbt.fact_order_line 
+        from dbt.fact_order_line
         group by 1,2,3,4
-        
+
         union all
-        
-        select 
-            cast(date_trunc(creation_date, day) as date) as date, 
-            'all', 
-            'all', 
-            'all', 
-            sum(total_price) gmv, 
-            sum(case when financial_status != 'pending' then total_price else null end) paid_gmv, 
+
+        select
+            cast(date_trunc(creation_date, day) as date) as date,
+            'all',
+            'all',
+            'all',
+            sum(total_price) gmv,
+            sum(case when financial_status != 'pending' then total_price else null end) paid_gmv,
             count(distinct order_id) count_orders,
             count(distinct case when financial_status != 'pending' then order_id else null end) count_paid_orders,
-        from dbt.fact_order_line 
+        from dbt.fact_order_line
         group by 1,2,3,4
         ) o
-    on k.date = o.`date` 
-    and k.utm_source = o.utm_source 
-    and k.utm_medium = o.utm_medium 
-    and k.utm_campaign = o.utm_campaign 
-    --and k.landing_page = o.landing_page 
-    --and k.channel = o.channel 
+    on k.date = o.`date`
+    and k.utm_source = o.utm_source
+    and k.utm_medium = o.utm_medium
+    and k.utm_campaign = o.utm_campaign
     group by 1,2,3,4
 )
 
