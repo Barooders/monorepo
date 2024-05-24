c7c0e236bf9dc3777bc9b5e04e471cf1f930ccaa
Clement Dessoude <clement@barooders.com>
2024-05-24 15:44:12 +0200

    ✨ upload multi-format images before storing product

--- packages/store/src/services/multi-format-image.ts
+++ packages/store/src/services/multi-format-image.ts
@@ -1,4 +1,6 @@
-import { IFileService, TransactionBaseService } from '@medusajs/medusa';
+import { IFileService, Logger, TransactionBaseService } from '@medusajs/medusa';
+import { S3 } from 'aws-sdk';
+import axios from 'axios';
 import fs from 'fs';
 import { parse } from 'path';
 import { ParsedPath } from 'path/posix';
@@ -6,6 +8,7 @@ import sharp from 'sharp';
 
 type InjectedDependencies = {
   fileService: IFileService;
+  logger: Logger;
 };
 
 const SIZE_CONFIG = {
@@ -25,12 +28,73 @@ const SIZE_CONFIG = {
 
 class MultiFormatImageService extends TransactionBaseService {
   protected fileService_: IFileService;
+  protected logger_: Logger;
+  protected s3: S3;
+  private BUCKET_NAME = 'barooders-medusa-staging';
+  private PATH_PREFIX = 'products';
 
-  constructor({ fileService }: InjectedDependencies) {
+  constructor({ fileService, logger }: InjectedDependencies) {
     // eslint-disable-next-line prefer-rest-params
     super(arguments[0]);
 
     this.fileService_ = fileService;
+    this.logger_ = logger;
+    this.s3 = new S3({
+      apiVersion: '2006-03-01',
+      accessKeyId: 'AKIAYMCN75NMHGB757M2',
+      secretAccessKey: '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',
+    });
+  }
+
+  private uploadFile(fileName: string, content: Buffer) {
+    const params = {
+      Bucket: this.BUCKET_NAME,
+      Key: `${this.PATH_PREFIX}/${fileName}`,
+      Body: content,
+    };
+
+    return this.s3.upload(params, function (err, data) {
+      if (err != null) {
+        throw err;
+      }
+      return data;
+    });
+  }
+
+  private publicUrl(fileName: string) {
+    return `https://${this.BUCKET_NAME}.s3.amazonaws.com/${fileName}`;
+  }
+
+  async multiFormatUploadFromUrl({
+    url,
+    fileName,
+  }: {
+    url: string;
+    fileName: string;
+  }): Promise<any> {
+    const input = (
+      await axios({
+        url,
+        responseType: 'arraybuffer',
+      })
+    ).data as Buffer;
+
+    const results = await Promise.all(
+      Object.keys(SIZE_CONFIG).map(async (size) => {
+        const { width, height } = SIZE_CONFIG[size];
+        const output = await sharp(input)
+          .resize(width, height)
+          .png()
+          .toBuffer();
+
+        return this.publicUrl(
+          (await this.uploadFile(`${fileName}-${size}.png`, output).promise())
+            .Key,
+        );
+      }),
+    );
+
+    return results;
   }
 
   async multiFormatUpload(file: Express.Multer.File): Promise<any> {
