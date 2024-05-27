81a50c08d3aad1e7fd3f93c90af691be29f20864
Clement Dessoude <clement@barooders.com>
2024-05-27 12:15:04 +0200

    💚 fix lint

--- packages/store/src/admin/utils/prepare-region.ts
+++ packages/store/src/admin/utils/prepare-region.ts
@@ -6,7 +6,7 @@ export default async function prepareRegions(client: Medusa) {
   let { regions } = await client.admin.regions.list();
   if (regions.length === 0) {
     let { store } = await client.admin.store.retrieve();
-    if (!store.currencies) {
+    if (store.currencies == null) {
       store = (
         await client.admin.store.update({
           currencies: ['eur'],
