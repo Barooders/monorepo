574b0101e767025361292e3c0b35ddf318ca7770
jeanphilippeds <jean-philippe@barooders.com>
2024-05-28 10:17:21 +0200

    :wrench: add family to product type

--- packages/backend/src/modules/pro-vendor/infrastructure/api/shopify/mappers/tuvalum-v2.mapper.ts
+++ packages/backend/src/modules/pro-vendor/infrastructure/api/shopify/mappers/tuvalum-v2.mapper.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { IMetafield, IProduct } from 'shopify-api-node';
-import { ShopifyDefaultMapper } from './default.mapper';
+import {
+  ShopifyDefaultMapper,
+  getMetafieldValueFromKey,
+} from './default.mapper';
 
 @Injectable()
 export class TuvalumV2Mapper extends ShopifyDefaultMapper {
   async getExternalCategory(
     { product_type }: IProduct,
-    _productMetafields: IMetafield[],
+    productMetafields: IMetafield[],
   ): Promise<string> {
-    return product_type;
+    return `${product_type} - ${getMetafieldValueFromKey('custitem_family', productMetafields)}`;
   }
 }
