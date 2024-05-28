3cbee9ff69b95cc759691020821b01b0d9977cdd
Clement Dessoude <clement@barooders.com>
2024-05-28 16:02:09 +0200

    🐛 put back redis configuration

--- packages/backend/src/libs/infrastructure/redis/redis.config.ts
+++ packages/backend/src/libs/infrastructure/redis/redis.config.ts
@@ -1,19 +1,25 @@
 import { BullModuleOptions } from '@nestjs/bull';
+import { get } from 'env-var';
 
 export const getRedisConfig = async (): Promise<BullModuleOptions> => {
-  // const connexionString = get('REDIS_URL').required().asString();
-  // const parsedConnexionString = connexionString.match(
-  //   /rediss{0,1}:\/\/(.*):(.*)@(.*):(.*)/,
-  // );
+  const connexionString = get('REDIS_URL').required().asString();
+  const parsedConnexionString = connexionString.match(
+    /rediss{0,1}:\/\/(.*):(.*)@(.*):(.*)/,
+  );
 
-  // if (!parsedConnexionString) {
-  //   throw new Error('Invalid REDIS_URL');
-  // }
+  if (!parsedConnexionString) {
+    throw new Error('Invalid REDIS_URL');
+  }
 
   return {
     redis: {
-      host: 'localhost',
-      port: 6379,
+      host: parsedConnexionString[3],
+      port: parseInt(parsedConnexionString[4]),
+      username: parsedConnexionString[1],
+      password: parsedConnexionString[2],
+      ...(connexionString.includes('rediss://') && {
+        tls: { rejectUnauthorized: false },
+      }),
     },
   };
 };
