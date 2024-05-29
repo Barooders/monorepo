6d858723c56fdc963d1ad02f56b26c466c0b796a
jeanphilippeds <jean-philippe@barooders.com>
2024-05-29 12:01:16 +0200

    :recycle: remove shopify id from dim_product_variant

--- packages/data-pipeline/models/analytics/datasets/shop/dim_product_variant.sql
+++ packages/data-pipeline/models/analytics/datasets/shop/dim_product_variant.sql
@@ -3,22 +3,24 @@
 
 with dim_product_variant as (
     select 
-        v.id ,
-        DATETIME(v.created_at, 'Europe/Paris') as creation_datetime,
-        date_trunc(DATETIME(v.created_at, 'Europe/Paris'), day) as creation_date,
-        v.product_id ,
+        b_pv.id as internal_id,
+        b_pv.merchantItemId as merchant_item_id,
+        DATETIME(b_pv.createdAt, 'Europe/Paris') as creation_datetime,
+        date_trunc(DATETIME(b_pv.createdAt, 'Europe/Paris'), day) as creation_date,
+        b_pv.productId as product_internal_id ,
         v.option_1 ,
         v.option_2 ,
         v.option_3 ,
         v.title ,
         v.sku ,
         v.grams ,
-        v.price,
-        v.compare_at_price,
-        v.inventory_quantity,
+        b_pv.priceInCents / 100 as price,
+        b_pv.compareAtPriceInCents / 100 as compare_at_price,
+        b_pv.quantity,
         v.requires_shipping as requires_shipping,
-    from shopify.product_variant v)
+    from backend__public.ProductVariant b_pv
+    left join shopify.product_variant v ON b_pv.shopifyId = v.id)
 
 select *
 from dim_product_variant
-order by 2 desc
+order by 3 desc
