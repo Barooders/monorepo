c7dde6bbdc781b5030c04c07048982482d568299
Clement Dessoude <clement@barooders.com>
2024-05-29 14:12:48 +0200

    ✨ Add attributes to medusa created product

--- packages/backend/src/modules/product/infrastructure/store/medusa.client.ts
+++ packages/backend/src/modules/product/infrastructure/store/medusa.client.ts
@@ -1,12 +1,19 @@
-import envConfig from '@config/env/env.config';
+import { CustomerRepository } from '@libs/domain/customer.repository';
 import { ProductStatus } from '@libs/domain/prisma.main.client';
 import {
   Product,
   ProductToStore,
   Variant,
 } from '@libs/domain/product.interface';
+import { getValidTags } from '@libs/domain/types';
 import { UUID } from '@libs/domain/value-objects';
-import { createHttpClient } from '@libs/infrastructure/http/clients';
+import { toCents } from '@libs/helpers/currency';
+import { medusaClient } from '@libs/infrastructure/medusa/client';
+import {
+  AdminPostProductsReq,
+  ProductStatus as MedusaProductStatus,
+} from '@medusajs/medusa';
+import { IPIMClient } from '@modules/product/domain/ports/pim.client';
 import {
   IStoreClient,
   ProductCreatedInStore,
@@ -18,31 +25,51 @@ import { ImageToUpload, ProductImage } from '@modules/product/domain/types';
 import { ProductStoreId } from '@modules/product/domain/value-objects/product-store-id.value-object';
 import { VariantStoreId } from '@modules/product/domain/value-objects/variant-store-id.value-object';
 import { Injectable, Logger } from '@nestjs/common';
+import compact from 'lodash/compact';
+import first from 'lodash/first';
+import uniq from 'lodash/uniq';
 import { ImageUploadsClient } from './image-uploads-client';
-import { CreateProductRequest, CreateProductResponse } from './medusa.dto';
 
-export const medusaClient = createHttpClient(
-  envConfig.externalServices.medusa.baseUrl,
-  {
-    headers: {
-      'x-medusa-access-token': `${envConfig.externalServices.medusa.apiKey}`,
-    },
-  },
-);
+const handle = (title: string): string => {
+  const slugified = title
+    .toLowerCase()
+    .replace(/ /g, '-')
+    .replace(/[^a-z0-9-]/g, '');
+
+  return `${slugified}-${Date.now()}`;
+};
+
+type ArrayElement<ArrayType extends unknown[] | undefined> =
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+type MedusaVariantRequest = ArrayElement<AdminPostProductsReq['variants']>;
 
 @Injectable()
 export class MedusaClient implements IStoreClient {
   private readonly logger = new Logger(MedusaClient.name);
 
-  constructor(private readonly imageUploadsClient: ImageUploadsClient) {}
+  constructor(
+    private customerRepository: CustomerRepository,
+    private readonly imageUploadsClient: ImageUploadsClient,
+    private pimClient: IPIMClient,
+  ) {}
 
   getProductDetails(productId: UUID): Promise<ProductDetails> {
     this.logger.log(`Getting product details for ${productId}`);
     throw new Error('Method not implemented.');
   }
 
+  // TODO: add vendor link
   async createProduct(product: ProductToStore): Promise<ProductCreatedInStore> {
     this.logger.log(`Creating product ${product.title}`);
+
+    const customer = await this.customerRepository.getCustomerFromVendorId(
+      product.vendorId,
+    );
+
+    if (!customer)
+      throw new Error(`Cannot find vendor with id: ${product.vendorId}`);
+
+    const { isPro } = customer;
 
     const imagesUrl = product.images
       ?.filter((image) => image.src !== undefined)
@@ -51,23 +78,70 @@ export class MedusaClient implements IStoreClient {
     const uploadedImages =
       await this.imageUploadsClient.uploadImages(imagesUrl);
 
-    const requestBody: CreateProductRequest = {
+    const tags = getValidTags(product.tags);
+
+    const {
+      attributes: { weight },
+    } = await this.pimClient.getPimProductType(product.product_type);
+    const productTypeId = await this.getOrCreateCategory(product.product_type);
+
+    const options = uniq(
+      product.variants.flatMap((variant) =>
+        variant.optionProperties.map((option) => option.key),
+      ),
+    );
+
+    const metadata: Record<string, unknown> = {
+      owner: isPro ? 'pro' : 'c2c',
+      source: product.source,
+    };
+    product.metafields.forEach(({ key, value }) => {
+      metadata[key] = value;
+    });
+
+    const productHandle = handle(product.title);
+    const requestBody: AdminPostProductsReq = {
       title: product.title,
+      is_giftcard: false,
+      discountable: true,
       description: product.body_html,
-      status: product.status === ProductStatus.ACTIVE ? 'published' : 'draft',
+      status: this.mapStatus(product.status),
       images: uploadedImages,
+      handle: productHandle,
+      tags: tags.map((tag) => ({ value: tag })),
+      weight,
+      categories: [{ id: productTypeId }],
+      options: options.map((name) => ({ title: name })),
+      variants: compact(
+        product.variants.map(
+          (variant): MedusaVariantRequest => ({
+            title: variant.title ?? 'Default',
+            prices: [
+              ...(variant.price !== undefined
+                ? [
+                    {
+                      amount: toCents(variant.price),
+                      currency_code: 'EUR',
+                    },
+                  ]
+                : []),
+            ],
+            options: variant.optionProperties.map((option) => ({
+              value: option.value,
+            })),
+          }),
+        ),
+      ),
+      metadata,
     };
 
     const { product: createdProduct } =
-      await medusaClient<CreateProductResponse>('/admin/products', {
-        method: 'POST',
-        data: requestBody,
-      });
+      await medusaClient.admin.products.create(requestBody);
 
     return {
       storeId: new ProductStoreId({ medusaId: createdProduct.id }),
       title: createdProduct.title,
-      handle: createdProduct.handle,
+      handle: productHandle,
       images: createdProduct.images.map((image) => ({
         src: image.url,
       })),
@@ -144,5 +218,37 @@ export class MedusaClient implements IStoreClient {
 
   cleanOldCommissions(): Promise<void> {
     throw new Error('Method not implemented.');
+  }
+
+  private mapStatus(status: ProductStatus): MedusaProductStatus {
+    return status === 'ACTIVE'
+      ? MedusaProductStatus.PUBLISHED
+      : status === 'ARCHIVED'
+        ? MedusaProductStatus.REJECTED
+        : MedusaProductStatus.DRAFT;
+  }
+
+  private async getOrCreateCategory(categoryName: string) {
+    try {
+      const response = await medusaClient.admin.productCategories.list({
+        q: categoryName,
+      });
+      const existingCategory = first(response.product_categories);
+      if (existingCategory) return existingCategory.id;
+
+      return await this.createCategory(categoryName);
+    } catch (e) {
+      return await this.createCategory(categoryName);
+    }
+  }
+
+  private async createCategory(categoryName: string) {
+    this.logger.log(`Creating category ${categoryName}`);
+    const createResponse = await medusaClient.admin.productCategories.create({
+      name: categoryName,
+      is_active: true,
+    });
+
+    return createResponse.product_category.id;
   }
 }
