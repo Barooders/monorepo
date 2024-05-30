9bc2a2ed0642cfecd701284a1a805959d3e6f23f
Nicolas Ngomai <nicolas.ngomai@gmail.com>
2024-05-30 16:13:01 +0200

    ✨ Add store id in Medusa product API

--- packages/store/src/api/index.ts
+++ packages/store/src/api/index.ts
@@ -1,5 +1,6 @@
 import { registerOverriddenValidators } from '@medusajs/medusa';
 import { AdminPostProductsReq as MedusaAdminPostProductsReq } from '@medusajs/medusa/dist/api/routes/admin/products/create-product';
+import { AdminGetProductsParams as MedusaAdminGetProductsParams } from '@medusajs/medusa/dist/api/routes/admin/products/list-products';
 import { IsString } from 'class-validator';
 
 class AdminPostProductsReq extends MedusaAdminPostProductsReq {
@@ -8,3 +9,10 @@ class AdminPostProductsReq extends MedusaAdminPostProductsReq {
 }
 
 registerOverriddenValidators(AdminPostProductsReq);
+
+class AdminGetProductsParams extends MedusaAdminGetProductsParams {
+  @IsString()
+  store_id: string;
+}
+
+registerOverriddenValidators(AdminGetProductsParams);
