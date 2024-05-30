995eec9059bd9be5ede82d72c43784df6a2db63f
jeanphilippeds <jean-philippe@barooders.com>
2024-05-30 15:20:45 +0200

    :recycle: remove public product variants from bigquery

--- packages/data-pipeline/models/analytics/datasets/shop/dim_product_variant.sql
+++ packages/data-pipeline/models/analytics/datasets/shop/dim_product_variant.sql
@@ -4,23 +4,24 @@
 with dim_product_variant as (
     select 
         b_pv.id as internal_id,
-        b_pv.shopifyId as shopify_id,
-        b_pv.merchantItemId as merchant_item_id,
+        b_pv.shopify_id,
+        b_pv.merchant_item_id,
         DATETIME(b_pv.createdAt, 'Europe/Paris') as creation_datetime,
         date_trunc(DATETIME(b_pv.createdAt, 'Europe/Paris'), day) as creation_date,
-        b_pv.productId as product_internal_id ,
-        v.option_1 ,
-        v.option_2 ,
-        v.option_3 ,
-        v.title ,
-        v.sku ,
-        v.grams ,
-        b_pv.priceInCents / 100 as price,
-        b_pv.compareAtPriceInCents / 100 as compare_at_price,
-        b_pv.quantity,
-        v.requires_shipping as requires_shipping,
-    from backend__public.ProductVariant b_pv
-    left join shopify.product_variant v ON b_pv.shopifyId = v.id)
+        b_pv.productId as product_internal_id,
+        b_epv.option1,
+        b_epv.option2,
+        b_epv.option3,
+        b_epv.title,
+        v.sku,
+        v.grams,
+        v.price,
+        v.compare_at_price,
+        b_epv.inventory_quantity,
+        b_epv.requiresShipping as requires_shipping,
+    from backend__dbt.store_base_product_variant b_pv
+    left join backend__dbt.store_exposed_product_variant b_epv on b_epv.id = b_pv.id
+    left join shopify.product_variant v ON b_pv.shopify_id = v.id)
 
 select *
 from dim_product_variant
