f8c601492cfc0eb04d6662bb4aa385badbdff147
Nicolas Ngomai <nicolas.ngomai@gmail.com>
2024-06-05 15:26:57 +0200

    🐛 Fix dbt queries

--- packages/dbt/models/store/store_discount.sql
+++ packages/dbt/models/store/store_discount.sql
@@ -13,10 +13,10 @@ SELECT
   d.code,
   CASE
     WHEN dr.type = 'fixed' THEN 'fixed_amount'
-    ELSE dr.type
+    ELSE dr.type::text
   END AS value_type,
   dr.value::float / 100 AS value, -- noqa: RF04, (ignore reserved keyword)
-  COALESCE(discount_condition_customer_group.customer_group_id IS NULL, FALSE) AS is_public -- noqa: RF02, (references should be qualified if select has more than one referenced table/view)
+  COALESCE(dcg.customer_group_id IS NULL, FALSE) AS is_public -- noqa: RF02, (references should be qualified if select has more than one referenced table/view)
 FROM medusa.discount AS d
 LEFT JOIN medusa.discount_rule AS dr ON d.rule_id = dr.id
 LEFT JOIN medusa.discount_condition AS dc ON dr.id = dc.discount_rule_id
