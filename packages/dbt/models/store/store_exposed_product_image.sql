39e7f543c22c1715e0e3f98d72778334f3f9c5cd
Nicolas Ngomai <nicolas.ngomai@gmail.com>
2024-06-05 12:07:48 +0200

    🗃️  Remove shopify from dbt

--- packages/dbt/models/store/store_exposed_product_image.sql
+++ packages/dbt/models/store/store_exposed_product_image.sql
@@ -1,26 +1,20 @@
 {{ config(
     materialized='incremental',
-    unique_key='shopify_id',
+    unique_key='medusa_id',
 	pre_hook='delete from {{this}}'
 ) }}
 
-WITH product_images AS (
-  SELECT
-    id AS product_id,
-    jsonb_array_elements(images) AS image
-  FROM airbyte_shopify.products
-)
-
 SELECT -- noqa: ST06, (Select wildcards then simple targets before calculations and aggregates)
   bp.id AS "productId",
-  (pi.image ->> 'id')::bigint AS "shopify_id",
-  (pi.image ->> 'width')::bigint AS width,
-  (pi.image ->> 'height')::bigint AS height,
-  (pi.image ->> 'position')::bigint AS position, -- noqa: RF04
-  pi.image ->> 'src' AS src,
-  pi.image ->> 'alt' AS alt,
+  i.id AS "medusa_id",
+  NULL AS width,
+  NULL AS height,
+  NULL AS position, -- noqa: RF04
+  i.url AS src,
+  NULL AS alt,
   current_date AS "syncDate"
-FROM product_images AS pi
+FROM medusa.product_images pi.image_id
+LEFT JOIN medusa.images i ON i.id
 INNER JOIN
   {{ ref('store_base_product') }} AS bp
-  ON pi.product_id = bp."shopifyId"
+  ON pi.product_id = bp."medusaId"
--- packages/dbt/models/store/store_exposed_product_tag.sql
+++ packages/dbt/models/store/store_exposed_product_tag.sql
@@ -4,21 +4,15 @@
   pre_hook='delete from {{this}}'
 ) }}
 
-WITH tags AS (
-  SELECT
-    id AS shopify_product_id,
-    string_to_table(tags, ', ') AS value -- noqa: RF04, (ignore reserved keyword)
-  FROM airbyte_shopify.products
-)
-
 SELECT -- noqa: ST06, (Select wildcards then simple targets before calculations and aggregates)
   bp.id AS "product_id",
-  split_part(t.value, ':', 1) AS tag,
-  t.value AS full_tag,
-  min(substring(t.value FROM position(':' IN t.value) + 1)) AS value -- noqa: RF04, (ignore reserved keyword)
-FROM tags AS t
+  split_part(tag.value, ':', 1) AS tag,
+  tag.value AS full_tag,
+  min(substring(tag.value FROM position(':' IN tag.value) + 1)) AS value -- noqa: RF04, (ignore reserved keyword)
+FROM medusa.product_tag AS tag
+INNER JOIN medusa.product_tags AS tags ON tag.id = tags.product_tag_id
 INNER JOIN
   {{ ref('store_base_product') }} AS bp
-  ON t.shopify_product_id = bp."shopifyId"
+  ON tags.product_id = bp."medusaId"
 WHERE value LIKE '%:%'
 GROUP BY 1, 2, 3
