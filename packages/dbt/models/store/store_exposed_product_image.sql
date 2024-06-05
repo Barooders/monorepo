853ba04db081db5317c90ec0beec153f58412ca4
Nicolas Ngomai <nicolas.ngomai@gmail.com>
2024-06-05 15:44:02 +0200

    🐛 Fix more dbt queries

--- packages/dbt/models/store/store_exposed_product_image.sql
+++ packages/dbt/models/store/store_exposed_product_image.sql
@@ -7,14 +7,14 @@
 SELECT -- noqa: ST06, (Select wildcards then simple targets before calculations and aggregates)
   bp.id AS "productId",
   i.id AS "medusa_id",
-  NULL AS width,
-  NULL AS height,
-  NULL AS position, -- noqa: RF04
+  -1 AS width,
+  -1 AS height,
+  -1 AS position, -- noqa: RF04
   i.url AS src,
   NULL AS alt,
   current_date AS "syncDate"
-FROM medusa.product_images pi
-LEFT JOIN medusa.images i ON i.id = pi.image_id
+FROM medusa.product_images AS pi
+LEFT JOIN medusa.image AS i ON pi.image_id = i.id
 INNER JOIN
   {{ ref('store_base_product') }} AS bp
   ON pi.product_id = bp."medusaId"
