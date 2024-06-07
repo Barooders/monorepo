7fde269dbf7bb7faf0e0f24a3c03231541a7a362
jeanphilippeds <jean-philippe@barooders.com>
2024-06-07 11:41:37 +0200

    :bug: fix first image

--- packages/dbt/models/store/store_exposed_product_image.sql
+++ packages/dbt/models/store/store_exposed_product_image.sql
@@ -4,17 +4,26 @@
 	pre_hook='delete from {{this}}'
 ) }}
 
+WITH ranked_images AS (
+  SELECT
+    pi.product_id,
+    pi.image_id,
+    i.url,
+    ROW_NUMBER() OVER (PARTITION BY pi.product_id ORDER BY pi.image_id) AS position -- Replace `pi.image_id` with appropriate column for ordering if needed
+  FROM medusa.product_images AS pi
+  LEFT JOIN medusa.image AS i ON pi.image_id = i.id
+)
+
 SELECT -- noqa: ST06, (Select wildcards then simple targets before calculations and aggregates)
   bp.id AS "productId",
-  i.id AS "medusa_id",
+  ri.image_id AS "medusa_id",
   -1 AS width,
   -1 AS height,
-  -1 AS position, -- noqa: RF04
-  i.url AS src,
+  ri.position,
+  ri.url AS src,
   NULL AS alt,
   current_date AS "syncDate"
-FROM medusa.product_images AS pi
-LEFT JOIN medusa.image AS i ON pi.image_id = i.id
+FROM ranked_images AS ri
 INNER JOIN
   {{ ref('store_base_product') }} AS bp
-  ON pi.product_id = bp."medusaId"
+  ON ri.product_id = bp."medusaId"
